62d05c9a2d5fdad8f18a12cea42d215c1c9f269a
Alyssa Ross <hi@alyssa.is>
2022-10-10 23:28:43 +0000

    host/rootfs: generate virtiofsd services
    The approach of having a template service for each virtiofsd comes
    from the work I need on Wayland support for Unikie, hence the Unikie
    copyright in etc/s6-rc/ext-rc-init/up.
    
    Signed-off-by: Alyssa Ross <hi@alyssa.is>
    Message-Id: <20221010232909.1953738-4-hi@alyssa.is>

--- host/rootfs/default.nix
+++ host/rootfs/default.nix
@@ -8,7 +8,7 @@ pkgs.pkgsStatic.callPackage (
 
 { lib, stdenvNoCC, nixos, runCommand, writeReferencesToFile, s6-rc, tar2ext4
 , busybox, cloud-hypervisor, cryptsetup, execline, e2fsprogs, jq, kmod
-, mdevd, s6, s6-linux-init, socat, util-linuxMinimal, xorg
+, mdevd, s6, s6-linux-init, socat, util-linuxMinimal, virtiofsd, xorg
 }:
 
 let
@@ -50,7 +50,7 @@ let
 
   packages = [
     cloud-hypervisor e2fsprogs execline jq kmod mdevd s6 s6-linux-init s6-rc
-    socat start-vm
+    socat start-vm virtiofsd
 
     (cryptsetup.override {
       programs = {
