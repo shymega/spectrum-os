6c0c95661b43075d8e66171d5ff4cd682aded8ca
Alyssa Ross <alyssa.ross@unikie.com>
2022-11-15 11:27:28 +0000

    nix/checks.nix: add rustfmt check
    Signed-off-by: Alyssa Ross <alyssa.ross@unikie.com>

--- nix/checks.nix
+++ nix/checks.nix
@@ -6,6 +6,24 @@
 {
   recurseForDerivations = true;
 
+  rustfmt = config.pkgs.callPackage (
+    { lib, runCommand, rustfmt }:
+    runCommand "spectrum-rustfmt" {
+      src = lib.cleanSourceWith {
+        filter = path: type:
+          (builtins.baseNameOf path != "build" && type == "directory")
+          || builtins.match ''.*[^/]\.rs'' path != null;
+        src = lib.cleanSource ../.;
+      };
+
+      nativeBuildInputs = [ rustfmt ];
+    } ''
+      shopt -s globstar
+      rustfmt --check $src/**/*.rs
+      touch $out
+    ''
+  ) {};
+
   shellcheck = config.pkgs.callPackage (
     { lib, runCommand, shellcheck }:
     runCommand "spectrum-shellcheck" {
