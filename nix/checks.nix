shymega/spectrum-os

commit 169fdd697771326fb111ab4532804a40e0fbeaa1

release.nix: run shellcheck on build scripts
This will make the binary cache build (which I expect to become more
CI-like over time) fail if shellcheck doesn't pass on any shell
scripts found in the tree.  This will help ensure we are sticking to
POSIX shell features and following shell scripting best practices.

At present, only scripts with names ending in ".sh" are processed, as
we don't have any others, and discovering extensionless scripts would
be quite a bit more complex.  We can worry about that when the time
comes.

Signed-off-by: Alyssa Ross <alyssa.ross@unikie.com>
Message-Id: <20221110112219.459660-2-alyssa.ross@unikie.com>
Reviewed-by: Henri Rosten <henri.rosten@unikie.com>

nix/checks.nix
@@ -1,0 +1,24 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2022 Unikie
+
+{ config ? import ../nix/eval-config.nix {} }:
+
+{
+  recurseForDerivations = true;
+
+  shellcheck = config.pkgs.callPackage (
+    { lib, runCommand, shellcheck }:
+    runCommand "spectrum-shellcheck" {
+      src = lib.cleanSourceWith {
+        filter = path: type:
+          type == "directory" || builtins.match ''.*[^/]\.sh'' path != null;
+        src = lib.cleanSource ../.;
+      };
+
+      nativeBuildInputs = [ shellcheck ];
+    } ''
+      shellcheck $src/**/*.sh
+      touch $out
+    ''
+  ) {};
+}

release.nix
@@ -12,5 +12,7 @@
 {
   doc = import ./Documentation { inherit config; };
 
+  checks = import nix/checks.nix { inherit config; };
+
   combined = import img/combined/run-vm.nix { inherit config; };
 }

commit e2cdde0af4cebd13ceacf4ce7ce70e689bbe9dbc

nix/checks.nix: shellcheck more files
I didn't realise that bash's ** feature (which matches any depth of
directories) wasn't enabled by default, because it appeared to work,
just didn't find all scripts.

Signed-off-by: Alyssa Ross <alyssa.ross@unikie.com>
Fixes: 169fdd6 ("release.nix: run shellcheck on build scripts")
Message-Id: <20221118100947.33597-2-alyssa.ross@unikie.com>
Reviewed-by: Henri Rosten <henri.rosten@unikie.com>

nix/checks.nix
@@ -11,12 +11,14 @@
     runCommand "spectrum-shellcheck" {
       src = lib.cleanSourceWith {
         filter = path: type:
-          type == "directory" || builtins.match ''.*[^/]\.sh'' path != null;
+          (builtins.baseNameOf path != "build" && type == "directory")
+          || builtins.match ''.*[^/]\.sh'' path != null;
         src = lib.cleanSource ../.;
       };
 
       nativeBuildInputs = [ shellcheck ];
     } ''
+      shopt -s globstar
       shellcheck $src/**/*.sh
       touch $out
     ''