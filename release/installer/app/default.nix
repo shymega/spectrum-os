a7d0bf29084f840bf87b19e9b3d9e4f44230f6d9
Alyssa Ross <hi@alyssa.is>
2023-04-17 22:18:02 +0000

    lib/nixpkgs.default.nix: update
    Signed-off-by: Alyssa Ross <hi@alyssa.is>

--- release/installer/app/default.nix
+++ release/installer/app/default.nix
@@ -12,21 +12,6 @@ in
 
 eos-installer.overrideAttrs ({ patches ? [], postPatch ? "", ... }: {
   patches = patches ++ [
-    (fetchpatch {
-      name = "finished-use-poweroff-from-PATH.patch";
-      url = "https://github.com/endlessm/eos-installer/commit/a537fde1f2bc6bcbcd86a6e926aeeba824583e19.patch";
-      sha256 = "0fl7254v78f9amzw774daap9rf46q6jw3pn7h4drj1jfqayk558j";
-    })
-    (fetchpatch {
-      name = "diskimage-find-names-for-uncompressed-image-files.patch";
-      url = "https://github.com/endlessm/eos-installer/commit/cb6f176ba0340a571efb8cb2f607d5d592b94c98.patch";
-      sha256 = "1q5chzln0l81fwj279ak1pixga2wiqj6v98qf67lv69y16176pzm";
-    })
-    (fetchpatch {
-      name = "Add-more-log-messages-for-invalid-disk-images.patch";
-      url = "https://github.com/endlessm/eos-installer/commit/9a3a0c219e4bb1190ac995d0bbaa20816dd0618d.patch";
-      sha256 = "0x22dlh1mqvlflr01iaqsb7b9vkcz06sbnfhcr5c6f3x2vzbp4jx";
-    })
     ./0001-gpt-disable-gpt-partition-attribute-55-check.patch
     ./0002-gpt-disable-partition-table-CRC-check.patch
     ./0003-install-remove-Endless-OS-ad.patch
