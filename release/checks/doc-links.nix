608cf1bb5d9f252e983404ab9547e627a37d3eb4
Alyssa Ross <hi@alyssa.is>
2023-05-01 17:07:51 +0000

    release/checks/doc-links.nix: switch to bozohttpd
    Newer versions of Ruby no longer include an HTTP server by default.
    As a side effect, this reduces the total wall clock time reported by
    wget from 2.8s to 0.07s!
    
    Signed-off-by: Alyssa Ross <hi@alyssa.is>

--- release/checks/doc-links.nix
+++ release/checks/doc-links.nix
@@ -2,15 +2,15 @@
 # SPDX-FileCopyrightText: 2022 Unikie
 
 import ../../lib/eval-config.nix ({ config, ... }:
-config.pkgs.callPackage ({ runCommand, ruby, wget }:
+config.pkgs.callPackage ({ runCommand, bozohttpd, wget }:
 
 runCommand "spectrum-doc-links" {
   doc = import ../../Documentation { inherit config; };
-  nativeBuildInputs = [ ruby wget ];
+  nativeBuildInputs = [ bozohttpd wget ];
 } ''
   mkdir root
   ln -s $doc root/doc
-  ruby -run -e httpd -- --port 4000 root &
+  httpd -bI 4000 root
   wget -r -nv --delete-after --no-parent --retry-connrefused http://localhost:4000/doc/
   touch $out
 ''
