c292b68e182413578e442a12a13a36325c01054c
Alyssa Ross <hi@alyssa.is>
2023-05-25 10:23:56 +0000

    release/combined: don't repeat image name
    Signed-off-by: Alyssa Ross <hi@alyssa.is>

--- release/combined/eosimages.nix
+++ release/combined/eosimages.nix
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: MIT
-# SPDX-FileCopyrightText: 2021-2022 Alyssa Ross <hi@alyssa.is>
+# SPDX-FileCopyrightText: 2021-2023 Alyssa Ross <hi@alyssa.is>
 
 import ../../lib/eval-config.nix ({ config, ... }: with config.pkgs;
 
@@ -10,7 +10,7 @@ runCommand "eosimages.img" {
 } ''
   mkdir dir
   cd dir
-  ln -s $image Spectrum-0.0-x86_64-generic.0.Live.img
+  ln -s $image $imageName
   sha256sum $imageName > $imageName.sha256
   tar -chf $NIX_BUILD_TOP/eosimages.tar *
   tar2ext4 -i $NIX_BUILD_TOP/eosimages.tar -o $out
