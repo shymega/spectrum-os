707a9d06d3d2785b54caba9b806799f14a230667
Alyssa Ross <hi@alyssa.is>
2023-05-27 12:06:38 +0000

    vm/app/mg.nix: remove suprious netvm dependency
    Signed-off-by: Alyssa Ross <hi@alyssa.is>

--- vm/app/mg.nix
+++ vm/app/mg.nix
@@ -4,7 +4,6 @@
 { config ? import ../../../nix/eval-config.nix {} }:
 
 import ../make-vm.nix { inherit config; } {
-  providers.net = [ "netvm" ];
   sharedDirs.virtiofs0.path = "/ext";
   run = config.pkgs.pkgsStatic.callPackage (
     { writeScript, mg }:
