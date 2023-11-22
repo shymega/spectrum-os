db6a8b1dc29f48ae5b2c53d3d04cf0043ee50ea7
Alyssa Ross <hi@alyssa.is>
2023-11-22 17:30:34 +0100

    release/checks/doc-anchors.nix: avoid import
    Documentation's src isn't special — it's the same src that we already
    have access to.  We could only check in Documentation's sourceRoot,
    but it's probably better not to anyway, since if AsciiDoc files did
    end up elsewhere, we'd want them to have valid links too.
    
    Signed-off-by: Alyssa Ross <hi@alyssa.is>

--- release/checks/doc-anchors.nix
+++ release/checks/doc-anchors.nix
@@ -1,12 +1,11 @@
 # SPDX-License-Identifier: MIT
 # SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
 
-import ../../lib/eval-config.nix ({ config, ... }:
+import ../../lib/eval-config.nix ({ config, src, ... }:
 config.pkgs.callPackage ({ runCommand }:
 
 runCommand "spectrum-doc-anchors" {} ''
-  cd ${(import ../../Documentation { inherit config; }).src}
-  ! grep --color=always -r xref:http
+  ! grep --color=always -r xref:http ${src}
   touch $out
 ''
 ) { })
