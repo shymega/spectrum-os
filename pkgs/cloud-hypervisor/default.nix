shymega/spectrum-os

commit 323e2bb59c1cb11963d4f69f7877654127e8b792

pkgs: move cloud-hypervisor patch into Spectrum
Previously, this lived in our custom Nixpkgs tree.  Since it would be
nice to move away from having one of those, we need to bring this into
Spectrum directly.

This goes in an overlay because if some package in Nixpkgs uses
Cloud Hypervisor, there's no point ending up with two Cloud Hypervisor
builds in our build closure, one patched and one not.

Signed-off-by: Alyssa Ross <hi@alyssa.is>

pkgs/cloud-hypervisor/default.nix
@@ -1,0 +1,61 @@
+# SPDX-FileCopyrightText: 2022 Unikie
+# SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
+# SPDX-License-Identifier: MIT
+
+import ../../lib/overlay-package.nix "cloud-hypervisor" ({ final, super }:
+
+final.rustPlatform.buildRustPackage {
+  inherit (super.cloud-hypervisor)
+    pname version src separateDebugInfo nativeBuildInputs buildInputs
+    OPENSSL_NO_VENDOR cargoTestFlags meta;
+
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "acpi_tables-0.1.0" = "sha256-OGJX05yNwE7zZzATs8y0EZ714+lB+FgSia0TygRwWAU=";
+      "kvm-bindings-0.6.0" = "sha256-wGdAuPwsgRIqx9dh0m+hC9A/Akz9qg9BM+p06Fi5ACM=";
+      "kvm-ioctls-0.13.0" = "sha256-jHnFGwBWnAa2lRu4a5eRNy1Y26NX5MV8alJ86VR++QE=";
+      "micro_http-0.1.0" = "sha256-wX35VsrO1vxQcGbOrP+yZm9vG0gcTZLe7gH7xuAa12w=";
+      "mshv-bindings-0.1.1" = "sha256-8fEWawNeJ96CczFoJD3cqCsrROEvh8wJ4I0ForwzTJY=";
+      "versionize_derive-0.1.4" = "sha256-oGuREJ5+FDs8ihmv99WmjIPpL2oPdOr4REk6+7cV/7o=";
+      "vfio-bindings-0.4.0" = "sha256-hGhfOE9q9sf/tzPuaAHOca+JKCutcm1Myu1Tt9spaIQ=";
+      "vfio_user-0.1.0" = "sha256-fAqvy3YTDKXQqtJR+R2nBCWIYe89zTwtbgvJfPLqs1Q=";
+      "vm-fdt-0.2.0" = "sha256-lKW4ZUraHomSDyxgNlD5qTaBTZqM0Fwhhh/08yhrjyE=";
+    };
+  };
+
+  vhost = final.fetchFromGitHub {
+    name = "vhost";
+    owner = "rust-vmm";
+    repo = "vhost";
+    rev = "vhost-user-backend-v0.10.1";
+    hash = "sha256-pq545s7sqE0GFFkEkAvKwFKLuRArNThmRFqEYS3nNVo=";
+  };
+
+  cargoPatches = super.cloud-hypervisor.cargoPatches or [] ++ [
+    ./0001-build-use-local-vhost.patch
+    ./0002-virtio-devices-add-a-GPU-device.patch
+  ];
+
+  vhostPatches = [
+    vhost/0001-vhost-fix-receiving-reply-payloads.patch
+    vhost/0002-vhost_user-add-shared-memory-region-support.patch
+    vhost/0003-vhost-user-add-protocol-flag-for-shmem.patch
+  ];
+
+  # Don't concatenate versions from super.cloud-hypervisor,
+  # because we'll get the versions from buildRustPackage twice.
+  postUnpack = ''
+    unpackFile $vhost
+    chmod -R +w vhost
+  '';
+
+  postPatch = ''
+    pushd ../vhost
+    for patch in $vhostPatches; do
+        echo applying patch $patch
+        patch -p1 < $patch
+    done
+    popd
+  '';
+})

pkgs/default.nix
@@ -1,32 +1,17 @@
 # SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
 # SPDX-License-Identifier: MIT
 
-let
-  customConfigPath = builtins.tryEval <spectrum-config>;
-in
-
-{ config ?
-  if customConfigPath.success then import customConfigPath.value
-  else if builtins.pathExists ../config.nix then import ../config.nix
-  else {}
-}:
+{ ... } @ args:
 
 let
-  default = import ../lib/config.default.nix;
-
-  callConfig = config: if builtins.typeOf config == "lambda" then config {
-    inherit default;
-  } else config;
-
-  fullConfig = default // callConfig config;
-
-  pkgs = fullConfig.pkgsFun fullConfig.pkgsArgs;
+  config = import ../lib/config.nix args;
+  pkgs = import ./overlaid.nix ({ elaboratedConfig = config; } // args);
 
   inherit (pkgs.lib)
     cleanSource cleanSourceWith hasSuffix makeScope optionalAttrs;
 
-  scope = self: {
-    config = fullConfig;
+  scope = self: let pkgs = self.callPackage ({ pkgs }: pkgs) {}; in {
+    inherit config;
 
     callSpectrumPackage =
       path: (import path { inherit (self) callPackage; }).override;
@@ -35,9 +20,12 @@ let
     rootfs = self.callSpectrumPackage ../host/rootfs {};
     start-vm = self.callSpectrumPackage ../host/start-vm {};
 
-    pkgsStatic = makeScope
-      (self.callPackage ({ pkgs }: pkgs) {}).pkgsStatic.newScope scope;
-    
+    # Packages from the overlay, so it's possible to build them from
+    # the CLI easily.
+    inherit (pkgs) cloud-hypervisor;
+
+    pkgsStatic = makeScope pkgs.pkgsStatic.newScope scope;
+
     src = cleanSourceWith {
       filter = path: type:
         path != toString ../Documentation/_site &&

lib/config.nix
@@ -1,0 +1,22 @@
+# SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
+# SPDX-License-Identifier: MIT
+
+let
+  customConfigPath = builtins.tryEval <spectrum-config>;
+in
+
+{ config ?
+  if customConfigPath.success then import customConfigPath.value
+  else if builtins.pathExists ../config.nix then import ../config.nix
+  else {}
+}:
+
+let
+  default = import ../lib/config.default.nix;
+
+  callConfig = config: if builtins.typeOf config == "lambda" then config {
+    inherit default;
+  } else config;
+in
+
+default // callConfig config

lib/nixpkgs.default.nix
@@ -1,9 +1,7 @@
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
 
-# Generated by scripts/update-nixpkgs.sh.
-
 import (builtins.fetchTarball {
-  url = "https://spectrum-os.org/git/nixpkgs/snapshot/nixpkgs-048a4cd441a59cbf89defb18bb45c9f0b4429b35.tar.gz";
-  sha256 = "0977gigkp803xv3ivly0gdc1ndj9k8msdzjafk172fxp7cpqakmj";
+  url = "https://spectrum-os.org/git/nixpkgs/snapshot/nixpkgs-c20cc4f1f089f4a93651a1b667299ddac73f59ba.tar.gz";
+  sha256 = "1l789rypsccsjbpqcvhwd3y4y2dr2a6hhm49x98g7m3rzc47cs34";
 })

lib/overlay-package.nix
@@ -1,0 +1,14 @@
+# SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
+# SPDX-License-Identifier: MIT
+
+attr: callback:
+
+{ ... } @ args:
+
+if args ? final && args ? super then
+  callback args
+else
+  # Auto-called from CLI.
+  # Since overlaid packages can affect other packages they depend on,
+  # we have to get the package out of the applied overlay.
+  (import ../pkgs/overlaid.nix args).${attr}

pkgs/cloud-hypervisor/0001-build-use-local-vhost.patch
@@ -1,0 +1,53 @@
+From 4c8d6c07213a9826545f5682b2d51cb84ffbb9ff Mon Sep 17 00:00:00 2001
+From: Alyssa Ross <alyssa.ross@unikie.com>
+Date: Wed, 28 Sep 2022 12:18:19 +0000
+Subject: [PATCH 1/2] build: use local vhost
+SPDX-FileCopyrightText: 2022 Unikie
+SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
+SPDX-License-Identifier: Apache-2.0 AND LicenseRef-BSD-3-Clause-Google
+
+Signed-off-by: Alyssa Ross <alyssa.ross@unikie.com>
+Signed-off-by: Alyssa Ross <hi@alyssa.is>
+---
+ Cargo.lock | 4 ----
+ Cargo.toml | 2 ++
+ 2 files changed, 2 insertions(+), 4 deletions(-)
+
+diff --git a/Cargo.lock b/Cargo.lock
+index 346e3f45..d591569b 100644
+--- a/Cargo.lock
++++ b/Cargo.lock
+@@ -2160,8 +2160,6 @@ dependencies = [
+ [[package]]
+ name = "vhost"
+ version = "0.8.1"
+-source = "registry+https://github.com/rust-lang/crates.io-index"
+-checksum = "61957aeb36daf0b00b87fff9c10dd28a161bd35ab157553d340d183b3d8756e6"
+ dependencies = [
+  "bitflags 1.3.2",
+  "libc",
+@@ -2172,8 +2170,6 @@ dependencies = [
+ [[package]]
+ name = "vhost-user-backend"
+ version = "0.10.1"
+-source = "registry+https://github.com/rust-lang/crates.io-index"
+-checksum = "ab069cdedaf18a0673766eb0a07a0f4ee3ed1b8e17fbfe4aafe5b988e2de1d01"
+ dependencies = [
+  "libc",
+  "log",
+diff --git a/Cargo.toml b/Cargo.toml
+index 0f575076..3b39bede 100644
+--- a/Cargo.toml
++++ b/Cargo.toml
+@@ -55,6 +55,8 @@ zbus = { version = "3.11.1", optional = true }
+ kvm-bindings = { git = "https://github.com/cloud-hypervisor/kvm-bindings", branch = "ch-v0.6.0-tdx" }
+ kvm-ioctls = { git = "https://github.com/rust-vmm/kvm-ioctls", branch = "main" }
+ versionize_derive = { git = "https://github.com/cloud-hypervisor/versionize_derive", branch = "ch" }
++vhost = { path = "../vhost/crates/vhost" }
++vhost-user-backend = { path = "../vhost/crates/vhost-user-backend" }
+ 
+ [dev-dependencies]
+ dirs = "5.0.0"
+-- 
+2.42.0
+

pkgs/cloud-hypervisor/Cargo.lock.license
@@ -1,0 +1,2 @@
+SPDX-FileCopyrightText: The Cloud Hypervisor Authors
+SPDX-License-Identifier: Apache-2.0 AND LicenseRef-BSD-3-Clause-Google

commit e54e5c780de3837c61990976bac3b2c341efb551

pkgs: fix pkgsStatic.cloud-hypervisor build
In static builds, all build inputs are moved to propagated build
inputs, so we need to inherit those as well.

Fixes: 323e2bb ("pkgs: move cloud-hypervisor patch into Spectrum")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

pkgs/cloud-hypervisor/default.nix
@@ -7,7 +7,7 @@ import ../../lib/overlay-package.nix "cloud-hypervisor" ({ final, super }:
 final.rustPlatform.buildRustPackage {
   inherit (super.cloud-hypervisor)
     pname version src separateDebugInfo nativeBuildInputs buildInputs
-    OPENSSL_NO_VENDOR cargoTestFlags meta;
+    propagatedBuildInputs OPENSSL_NO_VENDOR cargoTestFlags meta;
 
   cargoLock = {
     lockFile = ./Cargo.lock;