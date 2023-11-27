shymega/spectrum-os

commit 2950ca4914fadd6a3d3cd9940a3ef66dc25bb85b

scripts/update-nixpkgs.sh: use upstream Nixpkgs
Updating to nixos-unstable seems like the most sensible default, but
it can be overridden because sometimes we want to update to other
branches, for example if they contain a fix for a problem in
nixos-unstable.

Signed-off-by: Alyssa Ross <hi@alyssa.is>

scripts/update-nixpkgs.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p curl
+#! nix-shell -i bash -p curl jq
 
 # SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
 # SPDX-License-Identifier: EUPL-1.2+
@@ -15,10 +15,10 @@ out="$(mktemp -p "$root/lib" -t nixpkgs.default.nix.XXXXXXXXXX)"
 exec > "$out"
 
 rev="$(
-	curl -fsLS https://spectrum-os.org/git/nixpkgs.git/info/refs |
-	awk '$2 == "refs/heads/rootfs" { print $1 }'
+	curl -fsLS https://api.github.com/repos/NixOS/nixpkgs/branches/${1-nixos-unstable} |
+	jq -r .commit.sha
 )"
-url="https://spectrum-os.org/git/nixpkgs/snapshot/nixpkgs-$rev.tar.gz"
+url="https://github.com/NixOS/nixpkgs/archive/$rev.tar.gz"
 
 cat <<EOF
 # SPDX-License-Identifier: CC0-1.0

commit 109d87a0804fec3f9e07642099c12067d7b06277

scripts/update-nixpkgs.sh: fix shellcheck warning
Fixes: 2950ca4 ("scripts/update-nixpkgs.sh: use upstream Nixpkgs")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

scripts/update-nixpkgs.sh
@@ -15,7 +15,7 @@ out="$(mktemp -p "$root/lib" -t nixpkgs.default.nix.XXXXXXXXXX)"
 exec > "$out"
 
 rev="$(
-	curl -fsLS https://api.github.com/repos/NixOS/nixpkgs/branches/${1-nixos-unstable} |
+	curl -fsLS "https://api.github.com/repos/NixOS/nixpkgs/branches/${1-nixos-unstable}" |
 	jq -r .commit.sha
 )"
 url="https://github.com/NixOS/nixpkgs/archive/$rev.tar.gz"