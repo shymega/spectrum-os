shymega/spectrum-os

commit 5a5f8921458185ff1a11d27766f551290d4825ef

release/checks/wayland: also test cloud-hypervisor
Signed-off-by: Alyssa Ross <hi@alyssa.is>

release/checks/wayland/default.nix
@@ -15,10 +15,17 @@ config.pkgs.nixosTest ({ pkgs, ... }: {
   nodes.machine = { ... }: {
     hardware.opengl.enable = true;
 
+    systemd.services.cloud-hypervisor = {
+      after = [ "crosvm-gpu.service" "weston.service" ];
+      requires = [ "crosvm-gpu.service" "weston.service" ];
+      serviceConfig.ExecStart = "${pkgs.cloud-hypervisor}/bin/cloud-hypervisor --memory shared=on --disk path=${appvm}/img/appvm/blk/root.img,readonly=on --disk path=${run}/blk/run.img,readonly=on --cmdline \"console=ttyS0 root=PARTLABEL=root\" --gpu socket=/run/crosvm-gpu.sock --serial tty --console null --kernel ${appvm}/img/appvm/vmlinux";
+    };
+
     systemd.services.crosvm = {
       after = [ "crosvm-gpu.service" "weston.service" ];
       requires = [ "crosvm-gpu.service" "weston.service" ];
-      serviceConfig.ExecStart = "${pkgs.crosvm}/bin/crosvm run --disk ${appvm}/img/appvm/blk/root.img --disk ${run}/blk/run.img -p \"console=ttyS0 root=PARTLABEL=root\" --vhost-user-gpu /run/crosvm-gpu.sock --serial type=stdout,hardware=virtio-console,stdin=true ${appvm}/img/appvm/vmlinux";
+      serviceConfig.ExecStart = "${pkgs.crosvm}/bin/crosvm run -s /run/crosvm --disk ${appvm}/img/appvm/blk/root.img --disk ${run}/blk/run.img -p \"console=ttyS0 root=PARTLABEL=root\" --vhost-user-gpu /run/crosvm-gpu.sock --serial type=stdout,hardware=virtio-console,stdin=true ${appvm}/img/appvm/vmlinux";
+      serviceConfig.ExecStop = "${pkgs.crosvm}/bin/crosvm stop /run/crosvm";
     };
 
     systemd.services.crosvm-gpu = {
@@ -61,10 +68,14 @@ config.pkgs.nixosTest ({ pkgs, ... }: {
     machine.wait_for_unit('multi-user.target')
 
     machine.start_job('crosvm.service')
-
     machine.wait_for_unit('surface-notify-socket.service');
     machine.succeed('test "$(wc -c /run/surface-notify)" = "1 /run/surface-notify"', timeout=180)
+    machine.screenshot('crosvm')
+    machine.stop_job('crosvm-gpu.service')
+    machine.stop_job('crosvm.service')
 
-    machine.screenshot('weston')
+    machine.start_job('cloud-hypervisor.service')
+    machine.succeed('test "$(wc -c /run/surface-notify)" = "1 /run/surface-notify"', timeout=180)
+    machine.screenshot('cloud-hypervisor')
   '';
 }))

commit 8af86fe4f1ce32b34c434f236ff9cca0f58e6292

release/checks/wayland: fix false positives
Sometimes the application running inside the crosvm VM can commit
between wc exiting and the VM being stopped.  If this happens,
there'll be an extra byte in the fifo, which will interfere with the
second test.  To fix this, recreate the fifo to ensure it's empty at
the start of the Cloud Hypervisor test.

Fixes: 5a5f892 ("release/checks/wayland: also test cloud-hypervisor")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

release/checks/wayland/default.nix
@@ -50,13 +50,14 @@ nixosTest ({ lib, pkgs, ... }: {
 
     systemd.services.surface-notify-socket = {
       serviceConfig.ExecStart = "${pkgs.coreutils}/bin/mkfifo /run/surface-notify";
+      serviceConfig.ExecStop = "${pkgs.coreutils}/bin/rm -f /run/surface-notify";
       serviceConfig.RemainAfterExit = true;
       serviceConfig.Type = "oneshot";
     };
 
     systemd.services.weston = {
       after = [ "surface-notify-socket.service" ];
-      requires = [ "surface-notify-socket.service" ];
+      wants = [ "surface-notify-socket.service" ];
       environment.XDG_RUNTIME_DIR = "/run";
       environment.WAYLAND_DEBUG = "server";
       serviceConfig.ExecStart = "${lib.getExe pkgs.westonLite} --modules ${surface-notify}/lib/weston/surface-notify.so,systemd-notify.so";
@@ -75,6 +76,8 @@ nixosTest ({ lib, pkgs, ... }: {
     machine.stop_job('crosvm-gpu.service')
     machine.stop_job('crosvm.service')
 
+    machine.systemctl('restart surface-notify-socket')
+    machine.wait_for_unit('surface-notify-socket')
     machine.start_job('cloud-hypervisor.service')
     machine.succeed('test "$(wc -c /run/surface-notify)" = "1 /run/surface-notify"', timeout=180)
     machine.screenshot('cloud-hypervisor')