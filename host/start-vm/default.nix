shymega/spectrum-os

commit 009a0369c69cec24e0ffc43e4ba3d71d290b2bef

release/checks/start-vm.nix: init
This makes it possible to test basic start-vm functionality in a
simulated environment withouth having to start a whole Spectrum
system.

Signed-off-by: Alyssa Ross <hi@alyssa.is>

host/start-vm/default.nix
@@ -26,5 +26,9 @@ stdenv.mkDerivation (finalAttrs: {
       }
     );
   };
+
+  meta = {
+    mainProgram = "start-vm";
+  };
 })
 ) (_: {})

release/checks/start-vm.nix
@@ -1,0 +1,9 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
+
+import ../../lib/call-package.nix (
+{ callSpectrumPackage, runCommand }:
+
+runCommand "start-vm-test" {} ''
+  ${callSpectrumPackage ../../scripts/run-spectrum-vm.nix {}} > $out
+'') (_: {})

release/checks/pkg-tests.nix
@@ -10,4 +10,7 @@ import ../../lib/call-package.nix (
   lseek = lib.recurseIntoAttrs lseek.tests;
 
   start-vm = lib.recurseIntoAttrs start-vm.tests;
+
+  run-spectrum-vm = lib.recurseIntoAttrs
+    (callSpectrumPackage ../../scripts/run-spectrum-vm.nix {}).tests;
 }) (_: {})

scripts/run-spectrum-vm.c
@@ -1,0 +1,182 @@
+// SPDX-License-Identifier: EUPL-1.2+
+// SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
+
+#include <assert.h>
+#include <err.h>
+#include <errno.h>
+#include <sched.h>
+#include <signal.h>
+#include <spawn.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdnoreturn.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/mount.h>
+#include <sys/poll.h>
+#include <sys/prctl.h>
+#include <sys/stat.h>
+
+#include <linux/prctl.h>
+
+static const char *tmpdir(void)
+{
+	static char *d;
+	if (!d)
+		d = getenv("TMPDIR");
+	if (!d)
+		d = "/tmp";
+	return d;
+}
+
+static noreturn void exit_listener_main(int fd, const char *dir_path)
+{
+	struct pollfd pollfd = { .fd = fd, .events = 0, .revents = 0 };
+
+	if (signal(SIGINT, SIG_IGN) == SIG_ERR)
+		warn("ignoring SIGINT");
+	if (signal(SIGTERM, SIG_IGN) == SIG_ERR)
+		warn("ignoring SIGTERM");
+
+	// Wait for the other end of the pipe to be closed.
+	while (poll(&pollfd, 1, -1) == -1) {
+		if (errno == EINTR || errno == EWOULDBLOCK)
+			continue;
+
+		err(EXIT_FAILURE, "poll");
+	}
+	assert(pollfd.revents == POLLERR);
+
+	execlp("rm", "rm", "-rf", "--", dir_path, NULL);
+	err(EXIT_FAILURE, "exec rm");
+}
+
+static void exit_listener_setup(const char *dir_path)
+{
+	int fd[2];
+
+	if (pipe(fd) == -1)
+		err(EXIT_FAILURE, "pipe");
+
+	switch (fork()) {
+	case -1:
+		close(fd[0]);
+		close(fd[1]);
+		err(EXIT_FAILURE, "fork");
+	case 0:
+		close(fd[0]);
+		exit_listener_main(fd[1], dir_path);
+	default:
+		close(fd[1]);
+	}
+}
+
+static char *const virtiofsd_argv[] = {
+	"virtiofsd",
+	"--socket-path", "../vm-fs-virtiofs0/env/virtiofsd.sock",
+	"--sandbox", "none",
+	"--shared-dir", "/",
+	NULL
+};
+
+static void spawn_virtiofsd(void)
+{
+	sigset_t sigset;
+	pid_t ppid = getpid();
+
+	switch (fork()) {
+	case -1:
+		err(EXIT_FAILURE, "fork");
+	case 0:
+		if (prctl(PR_SET_PDEATHSIG, SIGTERM) == -1)
+			err(EXIT_FAILURE, "prctl PR_SET_DEATHSIG");
+		if (getppid() != ppid)
+			exit(EXIT_SUCCESS);
+		if (sigemptyset(&sigset) == -1)
+			err(EXIT_FAILURE, "sigemptyset");
+		if (sigaddset(&sigset, SIGINT) == -1)
+			err(EXIT_FAILURE, "sigaddset");
+		if (sigprocmask(SIG_BLOCK, &sigset, NULL) == -1)
+			err(EXIT_FAILURE, "sigprocmask");
+		execv(VIRTIOFSD_PATH, virtiofsd_argv);
+		err(EXIT_FAILURE, "exec " VIRTIOFSD_PATH);
+	}
+}
+
+int main(void)
+{
+	int fd;
+	char *dir_path;
+
+	if (asprintf(&dir_path, "%s/run-spectrum-vm.XXXXXX", tmpdir()) == -1)
+		err(EXIT_FAILURE, NULL);
+
+	if (!mkdtemp(dir_path))
+		err(EXIT_FAILURE, "mkdtemp");
+
+	exit_listener_setup(dir_path);
+
+	if (chdir(dir_path) == -1)
+		err(EXIT_FAILURE, "chdir %s", dir_path);
+
+	if (mkdir("bin", 0777) == -1)
+		err(EXIT_FAILURE, "mkdir bin");
+	if (mkdir("dev", 0777) == -1)
+		err(EXIT_FAILURE, "mkdir dev");
+	if (mkdir("nix", 0777) == -1)
+		err(EXIT_FAILURE, "mkdir nix");
+	if (mkdir("proc", 0777) == -1)
+		err(EXIT_FAILURE, "mkdir proc");
+	if (mkdir("run", 0777) == -1)
+		err(EXIT_FAILURE, "mkdir run");
+	if (mkdir("vm-fs-virtiofs0", 0777) == -1)
+		err(EXIT_FAILURE, "mkdir vm-fs-virtiofs0");
+	if (mkdir("vm-fs-virtiofs0/env", 0777) == -1)
+		err(EXIT_FAILURE, "mkdir vm-fs-virtiofs0/env");
+	if (mkdir("vm", 0777) == -1)
+		err(EXIT_FAILURE, "mkdir vm");
+
+	if (open("bin/cloud-hypervisor", O_CLOEXEC|O_CREAT|O_EXCL, 0) == -1)
+		err(EXIT_FAILURE, "create bin/cloud-hypervisor");
+
+	if (chdir("vm") == -1)
+		err(EXIT_FAILURE, "chdir vm");
+	if (mkdir("env", 0777) == -1)
+		err(EXIT_FAILURE, "mkdir env");
+	if (mkdir("data", 0777) == -1)
+		err(EXIT_FAILURE, "mkdir data");
+
+	if (symlink(CONFIG_PATH, "data/config") == -1)
+		err(EXIT_FAILURE, "symlink data/config -> " CONFIG_PATH);
+	if (symlink(APPVM_PATH, "../usr") == -1)
+		err(EXIT_FAILURE, "symlink ../usr -> " APPVM_PATH);
+
+	spawn_virtiofsd();
+
+	if ((fd = open("/dev/null", O_WRONLY)) == -1)
+		err(EXIT_FAILURE, "open /dev/null");
+	if (dup2(fd, 3) == -1)
+		err(EXIT_FAILURE, "dup2 %d -> 3", fd);
+	if (fd != 3)
+		close(fd);
+
+	if ((fd = open(START_VM_PATH, O_PATH)) == -1)
+		err(EXIT_FAILURE, "open " START_VM_PATH);
+
+	if (unshare(CLONE_NEWUSER|CLONE_NEWNS) == -1)
+		err(EXIT_FAILURE, "unshare");
+	if (mount(CLOUD_HYPERVISOR_PATH, "../bin/cloud-hypervisor", NULL, MS_BIND, NULL) == -1)
+		err(EXIT_FAILURE, "bind mount " CLOUD_HYPERVISOR_PATH " -> ../bin/cloud-hypervisor");
+	if (mount("/dev", "../dev", NULL, MS_BIND|MS_REC, NULL) == -1)
+		err(EXIT_FAILURE, "bind mount /dev -> ../dev");
+	if (mount("/nix", "../nix", NULL, MS_BIND|MS_REC, NULL) == -1)
+		err(EXIT_FAILURE, "bind mount /nix -> ../nix");
+	if (mount("/proc", "../proc", NULL, MS_BIND|MS_REC, NULL) == -1)
+		err(EXIT_FAILURE, "bind mount /proc -> ../proc");
+	if (chroot(dir_path) == -1)
+		err(EXIT_FAILURE, "chroot");
+
+	fexecve(fd, (char *const []){"start-vm", NULL}, (char *const []){"PATH=/bin", NULL});
+	err(EXIT_FAILURE, "exec " START_VM_PATH);
+}

scripts/run-spectrum-vm.nix
@@ -1,0 +1,36 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
+
+{ run ? ../vm/app/poweroff.nix, ... } @ args:
+
+import ../lib/call-package.nix (
+{ callSpectrumPackage, start-vm
+, lib, runCommand, runCommandCC, clang-tools, cloud-hypervisor, virtiofsd
+}:
+
+let
+  cflags = [
+    ''-D_GNU_SOURCE''
+    ''-DAPPVM_PATH="${callSpectrumPackage ../img/app {}}"''
+    ''-DCONFIG_PATH="${callSpectrumPackage run {}}"''
+    ''-DCLOUD_HYPERVISOR_PATH="${lib.getExe cloud-hypervisor}"''
+    ''-DSTART_VM_PATH="${lib.getExe start-vm}"''
+    ''-DVIRTIOFSD_PATH="${lib.getExe virtiofsd}"''
+  ];
+in
+
+runCommandCC "run-spectrum-vm" {
+  passthru.tests = {
+    clang-tidy = runCommand "run-spectrum-vm-clang-tidy" {
+      nativeBuildInputs = [ clang-tools ];
+    } ''
+      clang-tidy --warnings-as-errors='*' \
+          ${lib.escapeShellArgs (map (flag: "--extra-arg=${flag}") cflags)} \
+          ${./run-spectrum-vm.c}
+      touch $out
+    '';
+  };
+} ''
+  $CC -o $out ${lib.escapeShellArgs cflags} -Wall -Wpedantic \
+      ${./run-spectrum-vm.c}
+'') (_: {}) (removeAttrs args [ "run" ])

commit 6d0e73864d28794cdbd26ab7b37259ab0e1e044c

host/start-vm: add run-spectrum-vm to tests
release/checks/start-vm.nix wasn't actually imported anywhere, so it
didn't do anything unless it was built on its own.

We could have listed it in release/checks/default.nix, but I think it
makes more sense to have it in start-vm's passthru.tests, so it's more
obvious that it should be run when changing start-vm.

Fixes: 009a036 ("release/checks/start-vm.nix: init")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

host/start-vm/default.nix
@@ -2,7 +2,7 @@
 # SPDX-FileCopyrightText: 2022-2023 Alyssa Ross <hi@alyssa.is>
 
 import ../../lib/call-package.nix (
-{ src, lib, stdenv, meson, ninja, rustc, clippy }:
+{ src, lib, stdenv, meson, ninja, rustc, clippy, run-spectrum-vm }:
 
 stdenv.mkDerivation (finalAttrs: {
   name = "start-vm";
@@ -28,6 +28,8 @@ stdenv.mkDerivation (finalAttrs: {
         postBuild = ''touch $out && exit 0'';
       }
     );
+
+    run = run-spectrum-vm.override { start-vm = finalAttrs.finalPackage; };
   };
 
   meta = {

pkgs/default.nix
@@ -30,6 +30,7 @@ let
     lseek = self.callSpectrumPackage ../tools/lseek {};
     rootfs = self.callSpectrumPackage ../host/rootfs {};
     start-vm = self.callSpectrumPackage ../host/start-vm {};
+    run-spectrum-vm = self.callSpectrumPackage ../scripts/run-spectrum-vm.nix {};
 
     # Packages from the overlay, so it's possible to build them from
     # the CLI easily.

release/checks/start-vm.nix
@@ -1,9 +1,0 @@
-# SPDX-License-Identifier: MIT
-# SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
-
-import ../../lib/call-package.nix (
-{ callSpectrumPackage, runCommand }:
-
-runCommand "start-vm-test" {} ''
-  ${callSpectrumPackage ../../scripts/run-spectrum-vm.nix {}} > $out
-'') (_: {})