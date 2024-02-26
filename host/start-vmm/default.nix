shymega/spectrum-os

commit a2d362cf70ee73d0c0e96f0d86f8cb61b4596a0b

host: allow VMs to be powered off
Before this change, the s6 services for cloud-hypervisor and virtiofsd
were only started when a VM was started, and vm-stop would bring the
service down.  The problem with this was that if a VM powered itself
off, instead of being stopped on the host using vm-stop, the VM would
instantly be restarted by s6.

To fix this, we disentangle keeping cloud-hypervisor running from
keeping the VM running.  cloud-hypervisor will now always be running,
so s6 will never restart it in normal operation, but it won't be
running a VM until it's told to.  Accomplishing this means having
start-vmm (renamed from start-vm to reflect its new purpose) configure
the VM in cloud-hypervisor without booting it, which is only possible
using the API, not the command line.  As a result, start-vm now
depends on miniserde so that it can construct the VM config JSON
object required by the API.

The build of start-vm has been adjusted to accomodate the complexity
stemming from the new dependencies.  Tests are moved into passthru,
because the start-vm used in Spectrum should have panic=abort, but
tests need panic=unwind, and we can't use both in the same Meson
instance without duplicating the non-native dependencies.

We can't use s6-rc dependencies to automatically boot provider VMs in
this setup, so vm-start has been modified to recurse into provider VMs.

lsvm has been updated to check the Cloud Hypervisor API to see whether
a VM is running, rather than just checking to see whether the s6
service is up.

Because cloud-hypervisor is now to be started as early as possible, we
need to make the dependencies of ext-rc-init more precise, so that
cloud-hypervisor does not attempt to start before /dev/kvm or
/dev/net/tun is available.

We're not using Meson's support for Cargo subprojects yet, because it
currently always builds crates with all features enabled.

Signed-off-by: Alyssa Ross <hi@alyssa.is>

host/start-vmm/default.nix
@@ -1,0 +1,110 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2022-2023 Alyssa Ross <hi@alyssa.is>
+
+import ../../lib/call-package.nix (
+{ src, lib, stdenv, fetchCrate, fetchFromGitHub, fetchurl, buildPackages
+, meson, ninja, rustc, clippy, run-spectrum-vm
+}:
+
+let
+  packageCache = [
+    (fetchCrate {
+      pname = "itoa";
+      version = "1.0.10";
+      unpack = false;
+      hash = "sha256-saRtGhcdhlql+D+SaVdlyqBHqbTLriy/N9vWE6eT/Uw=";
+    })
+    (fetchurl {
+      name = "miniserde-0.1.37.tar.gz";
+      url = "https://github.com/dtolnay/miniserde/archive/0.1.37.tar.gz";
+      hash = "sha256-zE4WY6uI/7P7NaJyb2aZAnSL1rBSq5hAtx26151qUEk=";
+    })
+    (fetchCrate {
+      pname = "proc-macro2";
+      version = "1.0.78";
+      unpack = false;
+      hash = "sha256-4kIq1kXYnJn48+a4ip/eyn+r6sg2sQAjccQ2fI+YSq4=";
+    })
+    (fetchCrate {
+      pname = "quote";
+      version = "1.0.35";
+      unpack = false;
+      hash = "sha256-KR7Jq179k0qvUDpkZsXVJRU10QjudHRyw5d8xazIaO8=";
+    })
+    (fetchCrate {
+      pname = "ryu";
+      version = "1.0.17";
+      unpack = false;
+      hash = "sha256-6GaXyRYBmoWIyZtfrDzq107AtLgZcHpoL9TSP6DOG6E=";
+    })
+    (fetchCrate {
+      pname = "syn";
+      version = "2.0.41";
+      unpack = false;
+      hash = "sha256-RMiyjEd8w78OeWZWHjRgEw4SVfehz3GTEHXxxeen4mk=";
+    })
+    (fetchCrate {
+      pname = "unicode-ident";
+      version = "1.0.12";
+      unpack = false;
+      hash = "sha256-M1S5rD+uH/Z1XLbbU2g622YWNPZ1V5Qt6k+s6+wP7ks=";
+    })
+  ];
+in
+
+stdenv.mkDerivation (finalAttrs: {
+  name = "start-vmm";
+
+  src = lib.fileset.toSource {
+    root = ../..;
+    fileset = lib.fileset.intersection src ./.;
+  };
+  sourceRoot = "source/host/start-vmm";
+
+  depsBuildBuild = [ buildPackages.stdenv.cc ];
+  nativeBuildInputs = [ meson ninja rustc ];
+
+  postPatch = lib.concatMapStringsSep "\n" (crate: ''
+    mkdir -p subprojects/packagecache
+    ln -s ${crate} subprojects/packagecache/${crate.name}
+  '') packageCache;
+
+  preConfigure = ''
+    mesonFlagsArray+=(-Drust_args="-C panic=abort" -Dtests=false -Dwerror=true)
+  '';
+
+  passthru.tests = {
+    clippy = finalAttrs.finalPackage.overrideAttrs (
+      { name, nativeBuildInputs ? [], ... }:
+      {
+        name = "${name}-clippy";
+        nativeBuildInputs = nativeBuildInputs ++ [ clippy ];
+        RUSTC = "clippy-driver";
+        preConfigure = ''
+          # It's not currently possible to enable warnings only for
+          # non-subprojects without enumerating the subprojects.
+          # https://github.com/mesonbuild/meson/issues/9398#issuecomment-954094750
+          mesonFlagsArray+=(
+              -Dwerror=true
+              -Dproc-macro2:werror=false
+              -Dproc-macro2:warning_level=0
+          )
+        '';
+        postBuild = ''touch $out && exit 0'';
+      }
+    );
+
+    run = run-spectrum-vm.override { start-vmm = finalAttrs.finalPackage; };
+
+    tests = finalAttrs.finalPackage.overrideAttrs ({ name, ... }: {
+      name = "${name}-tests";
+      preConfigure = "";
+      doCheck = true;
+    });
+  };
+
+  meta = {
+    mainProgram = "start-vmm";
+  };
+})
+) (_: {})

host/rootfs/default.nix
@@ -6,7 +6,7 @@ import ../../lib/call-package.nix (
 { callSpectrumPackage, lseek, src, pkgsMusl, pkgsStatic, linux_latest }:
 pkgsStatic.callPackage (
 
-{ start-vm
+{ start-vmm
 , lib, stdenvNoCC, nixos, runCommand, writeReferencesToFile, erofs-utils, s6-rc
 , busybox, cloud-hypervisor, cryptsetup, execline, e2fsprogs, jq, kmod
 , mdevd, s6, s6-linux-init, socat, util-linuxMinimal, virtiofsd, xorg
@@ -44,7 +44,7 @@ let
 
   packages = [
     cloud-hypervisor e2fsprogs execline jq kmod mdevd
-    s6 s6-linux-init s6-rc socat start-vm virtiofsd
+    s6 s6-linux-init s6-rc socat start-vmm virtiofsd
 
     (cryptsetup.override {
       programs = {

host/start-vm/default.nix
@@ -1,39 +1,0 @@
-# SPDX-License-Identifier: MIT
-# SPDX-FileCopyrightText: 2022-2023 Alyssa Ross <hi@alyssa.is>
-
-import ../../lib/call-package.nix (
-{ src, lib, stdenv, meson, ninja, rustc, clippy, run-spectrum-vm }:
-
-stdenv.mkDerivation (finalAttrs: {
-  name = "start-vm";
-
-  src = lib.fileset.toSource {
-    root = ../..;
-    fileset = lib.fileset.intersection src ./.;
-  };
-  sourceRoot = "source/host/start-vm";
-
-  nativeBuildInputs = [ meson ninja rustc ];
-
-  mesonFlags = [ "-Dwerror=true" ];
-
-  doCheck = true;
-
-  passthru.tests = {
-    clippy = finalAttrs.finalPackage.overrideAttrs (
-      { nativeBuildInputs ? [], ... }:
-      {
-        nativeBuildInputs = nativeBuildInputs ++ [ clippy ];
-        RUSTC = "clippy-driver";
-        postBuild = ''touch $out && exit 0'';
-      }
-    );
-
-    run = run-spectrum-vm.override { start-vm = finalAttrs.finalPackage; };
-  };
-
-  meta = {
-    mainProgram = "start-vm";
-  };
-})
-) (_: {})

pkgs/default.nix
@@ -7,7 +7,15 @@ let
   config = import ../lib/config.nix args;
   pkgs = import ./overlaid.nix ({ elaboratedConfig = config; } // args);
 
-  inherit (pkgs.lib) cleanSource fileset makeScope optionalAttrs;
+  inherit (pkgs.lib) cleanSource fileset makeScope optionalAttrs sourceByRegex;
+
+  subprojects =
+    project:
+    let dir = project + "/subprojects"; in
+    fileset.difference dir (fileset.fromSource (sourceByRegex dir [
+      ".*\.wrap"
+      "packagefiles(/.*)?"
+    ]));
 
   makeScopeWithSplicing = pkgs: pkgs.makeScopeWithSplicing' {
     otherSplices = {
@@ -29,7 +37,7 @@ let
 
     lseek = self.callSpectrumPackage ../tools/lseek {};
     rootfs = self.callSpectrumPackage ../host/rootfs {};
-    start-vm = self.callSpectrumPackage ../host/start-vm {};
+    start-vmm = self.callSpectrumPackage ../host/start-vmm {};
     run-spectrum-vm = self.callSpectrumPackage ../scripts/run-spectrum-vm.nix {};
 
     # Packages from the overlay, so it's possible to build them from
@@ -40,7 +48,9 @@ let
 
     srcWithNix = fileset.difference
       (fileset.fromSource (cleanSource ../.))
-      (fileset.unions (map fileset.maybeMissing [
+      (fileset.unions ([
+        (subprojects ../host/start-vmm)
+      ] ++ map fileset.maybeMissing [
         ../Documentation/.jekyll-cache
         ../Documentation/_site
         ../Documentation/diagrams/stack.svg

commit d5127549674e5bb4d4ad33ccdd86a5ef14aa997e

host/start-vmm: remove unused input
Fixes: a2d362c ("host: allow VMs to be powered off")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

host/start-vmm/default.nix
@@ -2,7 +2,7 @@
 # SPDX-FileCopyrightText: 2022-2023 Alyssa Ross <hi@alyssa.is>
 
 import ../../lib/call-package.nix (
-{ src, lib, stdenv, fetchCrate, fetchFromGitHub, fetchurl, buildPackages
+{ src, lib, stdenv, fetchCrate, fetchurl, buildPackages
 , meson, ninja, rustc, clippy, run-spectrum-vm
 }:
 