shymega/spectrum-os

commit a2d362cf70ee73d0c0e96f0d86f8cb61b4596a0b

host: allow VMs to be powered off
Before this change, the s6 services for cloud-hypervisor and virtiofsd
were only started when a VM was started, and vm-stop would bring the
service down.  The problem with this was that if a VM powered itself
off, instead of being stopped on the host using vm-stop, the VM would
instantly be restarted by s6.

To fix this, we disentangle keeping cloud-hypervisor running from
keeping the VM running.  cloud-hypervisor will now always be running,
so s6 will never restart it in normal operation, but it won't be
running a VM until it's told to.  Accomplishing this means having
start-vmm (renamed from start-vm to reflect its new purpose) configure
the VM in cloud-hypervisor without booting it, which is only possible
using the API, not the command line.  As a result, start-vm now
depends on miniserde so that it can construct the VM config JSON
object required by the API.

The build of start-vm has been adjusted to accomodate the complexity
stemming from the new dependencies.  Tests are moved into passthru,
because the start-vm used in Spectrum should have panic=abort, but
tests need panic=unwind, and we can't use both in the same Meson
instance without duplicating the non-native dependencies.

We can't use s6-rc dependencies to automatically boot provider VMs in
this setup, so vm-start has been modified to recurse into provider VMs.

lsvm has been updated to check the Cloud Hypervisor API to see whether
a VM is running, rather than just checking to see whether the s6
service is up.

Because cloud-hypervisor is now to be started as early as possible, we
need to make the dependencies of ext-rc-init more precise, so that
cloud-hypervisor does not attempt to start before /dev/kvm or
/dev/net/tun is available.

We're not using Meson's support for Cargo subprojects yet, because it
currently always builds crates with all features enabled.

Signed-off-by: Alyssa Ross <hi@alyssa.is>

host/start-vmm/ch.rs
@@ -1,0 +1,220 @@
+// SPDX-License-Identifier: EUPL-1.2+
+// SPDX-FileCopyrightText: 2022-2023 Alyssa Ross <hi@alyssa.is>
+
+use std::ffi::{CStr, OsStr, OsString};
+use std::io::Write;
+use std::mem::take;
+use std::num::NonZeroI32;
+use std::os::raw::{c_char, c_int};
+use std::os::unix::prelude::*;
+use std::process::{Command, Stdio};
+
+use miniserde::{json, Serialize};
+
+use crate::net::MacAddress;
+
+// Trivially safe.
+const EINVAL: NonZeroI32 = unsafe { NonZeroI32::new_unchecked(22) };
+const EPERM: NonZeroI32 = unsafe { NonZeroI32::new_unchecked(1) };
+const EPROTO: NonZeroI32 = unsafe { NonZeroI32::new_unchecked(71) };
+
+#[derive(Serialize)]
+pub struct ConsoleConfig {
+    pub mode: &'static str,
+    pub file: Option<String>,
+}
+
+#[derive(Serialize)]
+pub struct DiskConfig {
+    pub path: String,
+    pub readonly: bool,
+}
+
+#[derive(Serialize)]
+pub struct FsConfig {
+    pub socket: String,
+    pub tag: String,
+}
+
+#[derive(Serialize)]
+pub struct GpuConfig {
+    pub socket: String,
+}
+
+#[derive(Serialize)]
+#[repr(C)]
+pub struct NetConfig {
+    pub fd: RawFd,
+    pub mac: MacAddress,
+}
+
+#[derive(Serialize)]
+pub struct MemoryConfig {
+    pub size: i64,
+    pub shared: bool,
+}
+
+#[derive(Serialize)]
+pub struct PayloadConfig {
+    pub kernel: String,
+    pub cmdline: &'static str,
+}
+
+#[derive(Serialize)]
+pub struct VmConfig {
+    pub console: ConsoleConfig,
+    pub disks: Vec<DiskConfig>,
+    pub fs: Vec<FsConfig>,
+    pub gpu: Vec<GpuConfig>,
+    pub memory: MemoryConfig,
+    pub net: Vec<NetConfig>,
+    pub payload: PayloadConfig,
+    pub serial: ConsoleConfig,
+}
+
+fn command(vm_name: &str, s: impl AsRef<OsStr>) -> Command {
+    let mut api_socket_path = OsString::from("/run/service/ext-vm-");
+    api_socket_path.push(vm_name);
+    api_socket_path.push("/env/cloud-hypervisor.sock");
+
+    let mut command = Command::new("ch-remote");
+    command.stdin(Stdio::null());
+    command.arg("--api-socket");
+    command.arg(api_socket_path);
+    command.arg(s);
+    command
+}
+
+pub fn create_vm(vm_name: &str, mut config: VmConfig) -> Result<(), String> {
+    // Net devices can't be created from file descriptors in vm.create.
+    // https://github.com/cloud-hypervisor/cloud-hypervisor/issues/5523
+    let nets = take(&mut config.net);
+
+    let mut ch_remote = command(vm_name, "create")
+        .args(["--", "-"])
+        .stdin(Stdio::piped())
+        .spawn()
+        .map_err(|e| format!("failed to start ch-remote: {e}"))?;
+
+    let json = json::to_string(&config);
+    write!(ch_remote.stdin.as_ref().unwrap(), "{}", json)
+        .map_err(|e| format!("writing to ch-remote's stdin: {e}"))?;
+
+    let status = ch_remote
+        .wait()
+        .map_err(|e| format!("waiting for ch-remote: {e}"))?;
+    if status.success() {
+    } else if let Some(code) = status.code() {
+        return Err(format!("ch-remote exited {code}"));
+    } else {
+        let signal = status.signal().unwrap();
+        return Err(format!("ch-remote killed by signal {signal}"));
+    }
+
+    for net in nets {
+        add_net(vm_name, &net).map_err(|e| format!("failed to add net: {e}"))?;
+    }
+
+    Ok(())
+}
+
+pub fn add_net(vm_name: &str, net: &NetConfig) -> Result<OsString, NonZeroI32> {
+    let mut ch_remote = command(vm_name, "add-net")
+        .arg(format!("fd={},mac={}", net.fd, net.mac))
+        .stdout(Stdio::piped())
+        .spawn()
+        .or(Err(EPERM))?;
+
+    let jq_out = match Command::new("jq")
+        .args(["-j", ".id"])
+        .stdin(ch_remote.stdout.take().unwrap())
+        .stderr(Stdio::inherit())
+        .output()
+    {
+        Ok(o) => o,
+        Err(_) => {
+            // Try not to leave a zombie.
+            let _ = ch_remote.kill();
+            let _ = ch_remote.wait();
+            return Err(EPERM);
+        }
+    };
+
+    if let Ok(ch_remote_status) = ch_remote.wait() {
+        if ch_remote_status.success() && jq_out.status.success() {
+            return Ok(OsString::from_vec(jq_out.stdout));
+        }
+    }
+
+    Err(EPROTO)
+}
+
+pub fn remove_device(vm_name: &str, device_id: &OsStr) -> Result<(), NonZeroI32> {
+    let ch_remote = command(vm_name, "remove-device")
+        .arg(device_id)
+        .status()
+        .or(Err(EPERM))?;
+
+    if ch_remote.success() {
+        Ok(())
+    } else {
+        Err(EPROTO)
+    }
+}
+
+/// # Safety
+///
+/// - `vm_name` must point to a valid C string.
+/// - `tap` must be a file descriptor describing an tap device.
+/// - `mac` must be a valid pointer.
+#[export_name = "ch_add_net"]
+unsafe extern "C" fn add_net_c(
+    vm_name: *const c_char,
+    net: &NetConfig,
+    id: *mut *mut OsString,
+) -> c_int {
+    let Ok(vm_name) = CStr::from_ptr(vm_name).to_str() else {
+        return EINVAL.into();
+    };
+
+    match add_net(vm_name, net) {
+        Err(e) => e.get(),
+        Ok(id_str) => {
+            if !id.is_null() {
+                let token = Box::into_raw(Box::new(id_str));
+                *id = token;
+            }
+            0
+        }
+    }
+}
+
+/// # Safety
+///
+/// - `vm_name` must point to a valid C string.
+/// - `id` must be a device ID obtained by calling `add_net_c`.  After
+///   calling `remove_device_c`, the pointer is no longer valid.
+#[export_name = "ch_remove_device"]
+unsafe extern "C" fn remove_device_c(vm_name: *const c_char, device_id: *mut OsString) -> c_int {
+    let Ok(vm_name) = CStr::from_ptr(vm_name).to_str() else {
+        return EINVAL.into();
+    };
+    let device_id = Box::from_raw(device_id);
+
+    if let Err(e) = remove_device(vm_name, device_id.as_ref()) {
+        e.get()
+    } else {
+        0
+    }
+}
+
+/// # Safety
+///
+/// `id` must be a device ID obtained by calling `add_net_c`.  After
+/// calling `device_free`, the pointer is no longer valid.
+#[export_name = "ch_device_free"]
+unsafe extern "C" fn device_free(id: *mut OsString) {
+    if !id.is_null() {
+        drop(Box::from_raw(id))
+    }
+}

host/start-vm/ch.rs
@@ -1,126 +1,0 @@
-// SPDX-License-Identifier: EUPL-1.2+
-// SPDX-FileCopyrightText: 2022 Alyssa Ross <hi@alyssa.is>
-
-use std::ffi::{CStr, OsStr, OsString};
-use std::num::NonZeroI32;
-use std::os::raw::{c_char, c_int};
-use std::os::unix::prelude::*;
-use std::process::{Command, Stdio};
-
-use crate::format_mac;
-
-// Trivially safe.
-const EPERM: NonZeroI32 = unsafe { NonZeroI32::new_unchecked(1) };
-const EPROTO: NonZeroI32 = unsafe { NonZeroI32::new_unchecked(71) };
-
-fn command(vm_name: &OsStr, s: impl AsRef<OsStr>) -> Command {
-    let mut api_socket_path = OsString::from("/run/service/ext-vm-");
-    api_socket_path.push(vm_name);
-    api_socket_path.push("/env/cloud-hypervisor.sock");
-
-    let mut command = Command::new("ch-remote");
-    command.stdin(Stdio::null());
-    command.arg("--api-socket");
-    command.arg(api_socket_path);
-    command.arg(s);
-    command
-}
-
-pub fn add_net(vm_name: &OsStr, tap: RawFd, mac: &str) -> Result<OsString, NonZeroI32> {
-    let mut ch_remote = command(vm_name, "add-net")
-        .arg(format!("fd={},mac={}", tap, mac))
-        .stdout(Stdio::piped())
-        .spawn()
-        .or(Err(EPERM))?;
-
-    let jq_out = match Command::new("jq")
-        .args(["-j", ".id"])
-        .stdin(ch_remote.stdout.take().unwrap())
-        .stderr(Stdio::inherit())
-        .output()
-    {
-        Ok(o) => o,
-        Err(_) => {
-            // Try not to leave a zombie.
-            let _ = ch_remote.kill();
-            let _ = ch_remote.wait();
-            return Err(EPERM);
-        }
-    };
-
-    if let Ok(ch_remote_status) = ch_remote.wait() {
-        if ch_remote_status.success() && jq_out.status.success() {
-            return Ok(OsString::from_vec(jq_out.stdout));
-        }
-    }
-
-    Err(EPROTO)
-}
-
-pub fn remove_device(vm_name: &OsStr, device_id: &OsStr) -> Result<(), NonZeroI32> {
-    let ch_remote = command(vm_name, "remove-device")
-        .arg(device_id)
-        .status()
-        .or(Err(EPERM))?;
-
-    if ch_remote.success() {
-        Ok(())
-    } else {
-        Err(EPROTO)
-    }
-}
-
-/// # Safety
-///
-/// - `vm_name` must point to a valid C string.
-/// - `tap` must be a file descriptor describing an tap device.
-/// - `mac` must be a valid pointer.
-#[export_name = "ch_add_net"]
-unsafe extern "C" fn add_net_c(
-    vm_name: *const c_char,
-    tap: RawFd,
-    mac: *const [u8; 6],
-    id: *mut *mut OsString,
-) -> c_int {
-    let vm_name = CStr::from_ptr(vm_name);
-    let mac = format_mac(&*mac);
-
-    match add_net(OsStr::from_bytes(vm_name.to_bytes()), tap, &mac) {
-        Err(e) => e.get(),
-        Ok(id_str) => {
-            if !id.is_null() {
-                let token = Box::into_raw(Box::new(id_str));
-                *id = token;
-            }
-            0
-        }
-    }
-}
-
-/// # Safety
-///
-/// - `vm_name` must point to a valid C string.
-/// - `id` must be a device ID obtained by calling `add_net_c`.  After
-///   calling `remove_device_c`, the pointer is no longer valid.
-#[export_name = "ch_remove_device"]
-unsafe extern "C" fn remove_device_c(vm_name: *const c_char, device_id: *mut OsString) -> c_int {
-    let vm_name = CStr::from_ptr(vm_name);
-    let device_id = Box::from_raw(device_id);
-
-    if let Err(e) = remove_device(OsStr::from_bytes(vm_name.to_bytes()), device_id.as_ref()) {
-        e.get()
-    } else {
-        0
-    }
-}
-
-/// # Safety
-///
-/// `id` must be a device ID obtained by calling `add_net_c`.  After
-/// calling `device_free`, the pointer is no longer valid.
-#[export_name = "ch_device_free"]
-unsafe extern "C" fn device_free(id: *mut OsString) {
-    if !id.is_null() {
-        drop(Box::from_raw(id))
-    }
-}

commit cb2004a178707074240b40b7425e955cb3c44df7

host/start-vmm: update Rust safety documentation
This wasn't updated when the signature of the function was changed.

Fixes: a2d362c ("host: allow VMs to be powered off")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

host/start-vmm/ch.rs
@@ -172,8 +172,7 @@ pub fn remove_device(vm_name: &str, device_id: &OsStr) -> Result<(), NonZeroI32>
 /// # Safety
 ///
 /// - `vm_name` must point to a valid C string.
-/// - `tap` must be a file descriptor describing an tap device.
-/// - `mac` must be a valid pointer.
+/// - `id` must be a valid pointer.
 #[export_name = "ch_add_net"]
 unsafe extern "C" fn add_net_c(
     vm_name: *const c_char,