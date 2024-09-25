shymega/spectrum-os

commit a2d362cf70ee73d0c0e96f0d86f8cb61b4596a0b

host: allow VMs to be powered off
Before this change, the s6 services for cloud-hypervisor and virtiofsd
were only started when a VM was started, and vm-stop would bring the
service down.  The problem with this was that if a VM powered itself
off, instead of being stopped on the host using vm-stop, the VM would
instantly be restarted by s6.

To fix this, we disentangle keeping cloud-hypervisor running from
keeping the VM running.  cloud-hypervisor will now always be running,
so s6 will never restart it in normal operation, but it won't be
running a VM until it's told to.  Accomplishing this means having
start-vmm (renamed from start-vm to reflect its new purpose) configure
the VM in cloud-hypervisor without booting it, which is only possible
using the API, not the command line.  As a result, start-vm now
depends on miniserde so that it can construct the VM config JSON
object required by the API.

The build of start-vm has been adjusted to accomodate the complexity
stemming from the new dependencies.  Tests are moved into passthru,
because the start-vm used in Spectrum should have panic=abort, but
tests need panic=unwind, and we can't use both in the same Meson
instance without duplicating the non-native dependencies.

We can't use s6-rc dependencies to automatically boot provider VMs in
this setup, so vm-start has been modified to recurse into provider VMs.

lsvm has been updated to check the Cloud Hypervisor API to see whether
a VM is running, rather than just checking to see whether the s6
service is up.

Because cloud-hypervisor is now to be started as early as possible, we
need to make the dependencies of ext-rc-init more precise, so that
cloud-hypervisor does not attempt to start before /dev/kvm or
/dev/net/tun is available.

We're not using Meson's support for Cargo subprojects yet, because it
currently always builds crates with all features enabled.

Signed-off-by: Alyssa Ross <hi@alyssa.is>

host/start-vmm/ch.h
@@ -1,11 +1,16 @@
 // SPDX-License-Identifier: EUPL-1.2+
-// SPDX-FileCopyrightText: 2022 Alyssa Ross <hi@alyssa.is>
+// SPDX-FileCopyrightText: 2022-2023 Alyssa Ross <hi@alyssa.is>
 
 #include <stdint.h>
 
 struct ch_device;
 
-int ch_add_net(const char *vm_name, int tap, const uint8_t mac[6],
+struct net_config {
+	int fd;
+	uint8_t mac[6];
+};
+
+int ch_add_net(const char *vm_name, const struct net_config *,
                struct ch_device **out);
 int ch_remove_device(const char *vm_name, struct ch_device *);
 

host/start-vmm/ch.rs
@@ -1,0 +1,220 @@
+// SPDX-License-Identifier: EUPL-1.2+
+// SPDX-FileCopyrightText: 2022-2023 Alyssa Ross <hi@alyssa.is>
+
+use std::ffi::{CStr, OsStr, OsString};
+use std::io::Write;
+use std::mem::take;
+use std::num::NonZeroI32;
+use std::os::raw::{c_char, c_int};
+use std::os::unix::prelude::*;
+use std::process::{Command, Stdio};
+
+use miniserde::{json, Serialize};
+
+use crate::net::MacAddress;
+
+// Trivially safe.
+const EINVAL: NonZeroI32 = unsafe { NonZeroI32::new_unchecked(22) };
+const EPERM: NonZeroI32 = unsafe { NonZeroI32::new_unchecked(1) };
+const EPROTO: NonZeroI32 = unsafe { NonZeroI32::new_unchecked(71) };
+
+#[derive(Serialize)]
+pub struct ConsoleConfig {
+    pub mode: &'static str,
+    pub file: Option<String>,
+}
+
+#[derive(Serialize)]
+pub struct DiskConfig {
+    pub path: String,
+    pub readonly: bool,
+}
+
+#[derive(Serialize)]
+pub struct FsConfig {
+    pub socket: String,
+    pub tag: String,
+}
+
+#[derive(Serialize)]
+pub struct GpuConfig {
+    pub socket: String,
+}
+
+#[derive(Serialize)]
+#[repr(C)]
+pub struct NetConfig {
+    pub fd: RawFd,
+    pub mac: MacAddress,
+}
+
+#[derive(Serialize)]
+pub struct MemoryConfig {
+    pub size: i64,
+    pub shared: bool,
+}
+
+#[derive(Serialize)]
+pub struct PayloadConfig {
+    pub kernel: String,
+    pub cmdline: &'static str,
+}
+
+#[derive(Serialize)]
+pub struct VmConfig {
+    pub console: ConsoleConfig,
+    pub disks: Vec<DiskConfig>,
+    pub fs: Vec<FsConfig>,
+    pub gpu: Vec<GpuConfig>,
+    pub memory: MemoryConfig,
+    pub net: Vec<NetConfig>,
+    pub payload: PayloadConfig,
+    pub serial: ConsoleConfig,
+}
+
+fn command(vm_name: &str, s: impl AsRef<OsStr>) -> Command {
+    let mut api_socket_path = OsString::from("/run/service/ext-vm-");
+    api_socket_path.push(vm_name);
+    api_socket_path.push("/env/cloud-hypervisor.sock");
+
+    let mut command = Command::new("ch-remote");
+    command.stdin(Stdio::null());
+    command.arg("--api-socket");
+    command.arg(api_socket_path);
+    command.arg(s);
+    command
+}
+
+pub fn create_vm(vm_name: &str, mut config: VmConfig) -> Result<(), String> {
+    // Net devices can't be created from file descriptors in vm.create.
+    // https://github.com/cloud-hypervisor/cloud-hypervisor/issues/5523
+    let nets = take(&mut config.net);
+
+    let mut ch_remote = command(vm_name, "create")
+        .args(["--", "-"])
+        .stdin(Stdio::piped())
+        .spawn()
+        .map_err(|e| format!("failed to start ch-remote: {e}"))?;
+
+    let json = json::to_string(&config);
+    write!(ch_remote.stdin.as_ref().unwrap(), "{}", json)
+        .map_err(|e| format!("writing to ch-remote's stdin: {e}"))?;
+
+    let status = ch_remote
+        .wait()
+        .map_err(|e| format!("waiting for ch-remote: {e}"))?;
+    if status.success() {
+    } else if let Some(code) = status.code() {
+        return Err(format!("ch-remote exited {code}"));
+    } else {
+        let signal = status.signal().unwrap();
+        return Err(format!("ch-remote killed by signal {signal}"));
+    }
+
+    for net in nets {
+        add_net(vm_name, &net).map_err(|e| format!("failed to add net: {e}"))?;
+    }
+
+    Ok(())
+}
+
+pub fn add_net(vm_name: &str, net: &NetConfig) -> Result<OsString, NonZeroI32> {
+    let mut ch_remote = command(vm_name, "add-net")
+        .arg(format!("fd={},mac={}", net.fd, net.mac))
+        .stdout(Stdio::piped())
+        .spawn()
+        .or(Err(EPERM))?;
+
+    let jq_out = match Command::new("jq")
+        .args(["-j", ".id"])
+        .stdin(ch_remote.stdout.take().unwrap())
+        .stderr(Stdio::inherit())
+        .output()
+    {
+        Ok(o) => o,
+        Err(_) => {
+            // Try not to leave a zombie.
+            let _ = ch_remote.kill();
+            let _ = ch_remote.wait();
+            return Err(EPERM);
+        }
+    };
+
+    if let Ok(ch_remote_status) = ch_remote.wait() {
+        if ch_remote_status.success() && jq_out.status.success() {
+            return Ok(OsString::from_vec(jq_out.stdout));
+        }
+    }
+
+    Err(EPROTO)
+}
+
+pub fn remove_device(vm_name: &str, device_id: &OsStr) -> Result<(), NonZeroI32> {
+    let ch_remote = command(vm_name, "remove-device")
+        .arg(device_id)
+        .status()
+        .or(Err(EPERM))?;
+
+    if ch_remote.success() {
+        Ok(())
+    } else {
+        Err(EPROTO)
+    }
+}
+
+/// # Safety
+///
+/// - `vm_name` must point to a valid C string.
+/// - `tap` must be a file descriptor describing an tap device.
+/// - `mac` must be a valid pointer.
+#[export_name = "ch_add_net"]
+unsafe extern "C" fn add_net_c(
+    vm_name: *const c_char,
+    net: &NetConfig,
+    id: *mut *mut OsString,
+) -> c_int {
+    let Ok(vm_name) = CStr::from_ptr(vm_name).to_str() else {
+        return EINVAL.into();
+    };
+
+    match add_net(vm_name, net) {
+        Err(e) => e.get(),
+        Ok(id_str) => {
+            if !id.is_null() {
+                let token = Box::into_raw(Box::new(id_str));
+                *id = token;
+            }
+            0
+        }
+    }
+}
+
+/// # Safety
+///
+/// - `vm_name` must point to a valid C string.
+/// - `id` must be a device ID obtained by calling `add_net_c`.  After
+///   calling `remove_device_c`, the pointer is no longer valid.
+#[export_name = "ch_remove_device"]
+unsafe extern "C" fn remove_device_c(vm_name: *const c_char, device_id: *mut OsString) -> c_int {
+    let Ok(vm_name) = CStr::from_ptr(vm_name).to_str() else {
+        return EINVAL.into();
+    };
+    let device_id = Box::from_raw(device_id);
+
+    if let Err(e) = remove_device(vm_name, device_id.as_ref()) {
+        e.get()
+    } else {
+        0
+    }
+}
+
+/// # Safety
+///
+/// `id` must be a device ID obtained by calling `add_net_c`.  After
+/// calling `device_free`, the pointer is no longer valid.
+#[export_name = "ch_device_free"]
+unsafe extern "C" fn device_free(id: *mut OsString) {
+    if !id.is_null() {
+        drop(Box::from_raw(id))
+    }
+}

commit 4d59577a1564cf797c2c003efce851949e9aa228

host/start-vmm: fix cleaning up when VMM exits
Cloud Hypervisor only assigns IDs to devices once the VM has booted.
Given VMs are all set up at the same time, this is very unlikely to be
the case.  When the VM hadn't booted, its ID was being recorded as the
empty string (because jq hadn't been given -e, so wouldn't fail when
there was no response from Cloud Hypervisor), which would then mean
that the router net device wouldn't be removed when the client VMM
exited.

To fix this, assign IDs based on the tap name, rather than expecting
Cloud Hypervisor to do it.  This way, it doesn't matter that the
router VM isn't booted when the client VM is set up.

Fixes: a2d362c ("host: allow VMs to be powered off")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

host/start-vmm/ch.h
@@ -3,17 +3,17 @@
 
 #include <stdint.h>
 
+#include <net/if.h>
+
 struct ch_device;
 
 struct net_config {
 	int fd;
+	char id[IFNAMSIZ];
 	uint8_t mac[6];
 };
 
-int ch_add_net(const char vm_name[static 1], const struct net_config[static 1],
-               struct ch_device **out);
+int ch_add_net(const char vm_name[static 1], const struct net_config[static 1]);
 
-[[gnu::nonnull]]
-int ch_remove_device(const char vm_name[static 1], struct ch_device *);
-
-void ch_device_free(struct ch_device *);
+int ch_remove_device(const char vm_name[static 1],
+		     const char device_id[static 1]);

host/start-vmm/ch.rs
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: EUPL-1.2+
 // SPDX-FileCopyrightText: 2022-2024 Alyssa Ross <hi@alyssa.is>
 
+use std::convert::{TryFrom, TryInto};
 use std::ffi::{CStr, OsStr, OsString};
 use std::io::Write;
 use std::mem::take;
@@ -9,6 +10,7 @@ use std::os::raw::{c_char, c_int};
 use std::os::unix::prelude::*;
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
+use std::string::FromUtf8Error;
 
 use miniserde::{json, Serialize};
 
@@ -43,9 +45,9 @@ pub struct GpuConfig {
 }
 
 #[derive(Serialize)]
-#[repr(C)]
 pub struct NetConfig {
     pub fd: RawFd,
+    pub id: String,
     pub mac: MacAddress,
 }
 
@@ -111,12 +113,13 @@ pub fn create_vm(vm_name: &str, mut config: VmConfig) -> Result<(), String> {
     let status = ch_remote
         .wait()
         .map_err(|e| format!("waiting for ch-remote: {e}"))?;
-    if status.success() {
-    } else if let Some(code) = status.code() {
-        return Err(format!("ch-remote exited {code}"));
-    } else {
-        let signal = status.signal().unwrap();
-        return Err(format!("ch-remote killed by signal {signal}"));
+    if !status.success() {
+        if let Some(code) = status.code() {
+            return Err(format!("ch-remote exited {code}"));
+        } else {
+            let signal = status.signal().unwrap();
+            return Err(format!("ch-remote killed by signal {signal}"));
+        }
     }
 
     for net in nets {
@@ -126,31 +129,16 @@ pub fn create_vm(vm_name: &str, mut config: VmConfig) -> Result<(), String> {
     Ok(())
 }
 
-pub fn add_net(vm_name: &str, net: &NetConfig) -> Result<OsString, NonZeroI32> {
+pub fn add_net(vm_name: &str, net: &NetConfig) -> Result<(), NonZeroI32> {
     let mut ch_remote = command(vm_name, "add-net")
-        .arg(format!("fd={},mac={}", net.fd, net.mac))
+        .arg(format!("fd={},id={},mac={}", net.fd, net.id, net.mac))
         .stdout(Stdio::piped())
         .spawn()
         .or(Err(EPERM))?;
 
-    let jq_out = match Command::new("jq")
-        .args(["-j", ".id"])
-        .stdin(ch_remote.stdout.take().unwrap())
-        .stderr(Stdio::inherit())
-        .output()
-    {
-        Ok(o) => o,
-        Err(_) => {
-            // Try not to leave a zombie.
-            let _ = ch_remote.kill();
-            let _ = ch_remote.wait();
-            return Err(EPERM);
-        }
-    };
-
     if let Ok(ch_remote_status) = ch_remote.wait() {
-        if ch_remote_status.success() && jq_out.status.success() {
-            return Ok(OsString::from_vec(jq_out.stdout));
+        if ch_remote_status.success() {
+            return Ok(());
         }
     }
 
@@ -170,44 +158,65 @@ pub fn remove_device(vm_name: &str, device_id: &OsStr) -> Result<(), NonZeroI32>
     }
 }
 
-/// # Safety
-///
-/// - `vm_name` must point to a valid C string.
-#[export_name = "ch_add_net"]
-unsafe extern "C" fn add_net_c(
-    vm_name: *const c_char,
-    net: &NetConfig,
-    id: Option<&mut *mut OsString>,
-) -> c_int {
-    let Ok(vm_name) = CStr::from_ptr(vm_name).to_str() else {
-        return EINVAL.into();
-    };
+#[repr(C)]
+pub struct NetConfigC {
+    pub fd: RawFd,
+    pub id: [u8; 16],
+    pub mac: MacAddress,
+}
 
-    match add_net(vm_name, net) {
-        Err(e) => e.get(),
-        Ok(id_str) => {
-            if let Some(id) = id {
-                let token = Box::into_raw(Box::new(id_str));
-                *id = token;
-            }
-            0
-        }
+impl<'a> TryFrom<&'a NetConfigC> for NetConfig {
+    type Error = FromUtf8Error;
+
+    fn try_from(c: &'a NetConfigC) -> Result<NetConfig, Self::Error> {
+        let nul_index = c.id.iter().position(|&c| c == 0).unwrap_or(c.id.len());
+        Ok(NetConfig {
+            fd: c.fd,
+            id: String::from_utf8(c.id[..nul_index].to_vec())?,
+            mac: c.mac,
+        })
+    }
+}
+
+impl TryFrom<NetConfigC> for NetConfig {
+    type Error = FromUtf8Error;
+
+    fn try_from(c: NetConfigC) -> Result<NetConfig, Self::Error> {
+        Self::try_from(&c)
     }
 }
 
 /// # Safety
 ///
 /// - `vm_name` must point to a valid C string.
-/// - `device_id` must be a device ID obtained by calling `add_net_c`.  After
-///   calling `remove_device_c`, the pointer is no longer valid.
-#[export_name = "ch_remove_device"]
-unsafe extern "C" fn remove_device_c(vm_name: *const c_char, device_id: &mut OsString) -> c_int {
+/// - `net.fd` must be a valid file descriptor.
+/// - `net.id` must point to a valid C string.
+#[export_name = "ch_add_net"]
+unsafe extern "C" fn add_net_c(vm_name: *const c_char, net: &NetConfigC) -> c_int {
     let Ok(vm_name) = CStr::from_ptr(vm_name).to_str() else {
         return EINVAL.into();
     };
-    let device_id = Box::from_raw(device_id);
 
-    if let Err(e) = remove_device(vm_name, device_id.as_ref()) {
+    if let Err(e) = add_net(vm_name, &net.try_into().unwrap()) {
+        e.get()
+    } else {
+        0
+    }
+}
+
+/// # Safety
+///
+/// - `vm_name` must point to a valid C string.
+/// - `device_id` must point to a valid C string.
+#[export_name = "ch_remove_device"]
+unsafe extern "C" fn remove_device_c(vm_name: *const c_char, device_id: *const c_char) -> c_int {
+    let Ok(vm_name) = CStr::from_ptr(vm_name).to_str() else {
+        return EINVAL.into();
+    };
+
+    let device_id = OsStr::from_bytes(CStr::from_ptr(device_id).to_bytes());
+
+    if let Err(e) = remove_device(vm_name, device_id) {
         e.get()
     } else {
         0

host/start-vmm/lib.rs
@@ -8,6 +8,7 @@ mod s6;
 mod unix;
 
 use std::borrow::Cow;
+use std::convert::TryInto;
 use std::env::args_os;
 use std::ffi::{CString, OsStr};
 use std::fs::remove_file;
@@ -136,7 +137,7 @@ pub fn vm_config(vm_name: &str, config_root: &Path) -> Result<VmConfig, String> 
                         return Err(format!("setting up networking failed: {e}"));
                     }
 
-                    Ok(net)
+                    Ok(net.try_into().unwrap())
                 })
                 // TODO: to support multiple net providers, we'll need
                 // a better naming scheme for tap and bridge devices.

host/start-vmm/net.c
@@ -7,7 +7,6 @@
 #include <assert.h>
 #include <err.h>
 #include <errno.h>
-#include <net/if.h>
 #include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,14 +19,13 @@
 #include <linux/if_tun.h>
 
 static int setup_tap(const char bridge_name[static 1],
-		     const char tap_prefix[static 1])
+		     const char tap_prefix[static 1],
+		     char tap_name[static IFNAMSIZ])
 {
 	int fd;
-	char tap_name[IFNAMSIZ];
 
 	// We assume ≤16-bit pids.
-	if (snprintf(tap_name, sizeof tap_name, "%s%d",
-	             tap_prefix, getpid()) == -1)
+	if (snprintf(tap_name, IFNAMSIZ, "%s%d", tap_prefix, getpid()) == -1)
 		return -1;
 	if ((fd = tap_open(tap_name, IFF_NO_PI|IFF_VNET_HDR|IFF_TUN_EXCL)) == -1)
 		goto out;
@@ -44,39 +42,41 @@ out:
 	return fd;
 }
 
-static int client_net_setup(const char bridge_name[static 1])
+static int client_net_setup(const char bridge_name[static 1],
+			    char tap_name[static IFNAMSIZ])
 {
-	return setup_tap(bridge_name, "client");
+	return setup_tap(bridge_name, "client", tap_name);
 }
 
 static int router_net_setup(const char bridge_name[static 1],
 			    const char router_vm_name[static 1],
                             const uint8_t mac[6],
-			    struct ch_device *out[static 1])
+			    char device_id_out[static IFNAMSIZ])
 {
 	struct net_config net;
 	int e;
 
 	memcpy(&net.mac, mac, sizeof net.mac);
-	if ((net.fd = setup_tap(bridge_name, "router")) == -1)
+	if ((net.fd = setup_tap(bridge_name, "router", net.id)) == -1)
 		return -1;
 
-	e = ch_add_net(router_vm_name, &net, out);
+	e = ch_add_net(router_vm_name, &net);
 	close(net.fd);
-	if (!e)
+	if (!e) {
+		strcpy(device_id_out, net.id);
 		return 0;
+	}
 	errno = e;
 	return -1;
 }
 
-[[gnu::nonnull]]
 static int router_net_cleanup(pid_t pid, const char vm_name[static 1],
-                              struct ch_device *vm_net_device)
+                              const char device_id[static 1])
 {
 	int e;
 	char name[IFNAMSIZ], newname[IFNAMSIZ], brname[IFNAMSIZ];
 
-	if ((e = ch_remove_device(vm_name, vm_net_device))) {
+	if ((e = ch_remove_device(vm_name, device_id))) {
 		errno = e;
 		return -1;
 	}
@@ -104,10 +104,9 @@ static int bridge_cleanup(pid_t pid)
 	return bridge_delete(name);
 }
 
-[[gnu::nonnull]]
 static noreturn void exit_listener_main(int fd, pid_t pid,
                                         const char router_vm_name[static 1],
-                                        struct ch_device *router_vm_net_device)
+                                        const char router_device_id[static 1])
 {
 	// Wait for the other end of the pipe to be closed.
 	int status = EXIT_SUCCESS;
@@ -120,8 +119,7 @@ static noreturn void exit_listener_main(int fd, pid_t pid,
 	}
 	assert(pollfd.revents == POLLERR);
 
-	if (router_net_cleanup(pid, router_vm_name,
-	                       router_vm_net_device) == -1) {
+	if (router_net_cleanup(pid, router_vm_name, router_device_id) == -1) {
 		warn("cleaning up router tap");
 		status = EXIT_FAILURE;
 	}
@@ -133,9 +131,8 @@ static noreturn void exit_listener_main(int fd, pid_t pid,
 	exit(status);
 }
 
-[[gnu::nonnull]]
 static int exit_listener_setup(const char router_vm_name[static 1],
-                               struct ch_device *router_vm_net_device)
+                               const char router_device_id[static 1])
 {
 	pid_t pid = getpid();
 	int fd[2];
@@ -151,7 +148,7 @@ static int exit_listener_setup(const char router_vm_name[static 1],
 	case 0:
 		close(fd[0]);
 		exit_listener_main(fd[1], pid, router_vm_name,
-		                   router_vm_net_device);
+				   router_device_id);
 	default:
 		close(fd[1]);
 		return 0;
@@ -160,9 +157,8 @@ static int exit_listener_setup(const char router_vm_name[static 1],
 
 struct net_config net_setup(const char router_vm_name[static 1])
 {
-	struct ch_device *router_vm_net_device = NULL;
 	struct net_config r = { .fd = -1, .mac = { 0 } };
-	char bridge_name[IFNAMSIZ];
+	char bridge_name[IFNAMSIZ], router_device_id[IFNAMSIZ] = { 0 };
 	pid_t pid = getpid();
 	// We assume ≤16-bit pids.
 	uint8_t router_mac[6] = { 0x0A, 0xB3, 0xEC, 0x80, pid >> 8, pid };
@@ -174,30 +170,28 @@ struct net_config net_setup(const char router_vm_name[static 1])
 		return r;
 
 	if (bridge_add(bridge_name) == -1)
-		goto out;
+		return r;
 	if (if_up(bridge_name) == -1)
 		goto fail_bridge;
 
-	if ((r.fd = client_net_setup(bridge_name)) == -1)
+	if ((r.fd = client_net_setup(bridge_name, r.id)) == -1)
 		goto fail_bridge;
 
 	if (router_net_setup(bridge_name, router_vm_name, router_mac,
-	                     &router_vm_net_device) == -1)
+	                     router_device_id) == -1)
 		goto fail_bridge;
 
 	// Set up a process that will listen for this process dying,
 	// and remove the interface from the netvm, and delete the
 	// bridge.
-	if (exit_listener_setup(router_vm_name, router_vm_net_device) == -1)
+	if (exit_listener_setup(router_vm_name, router_device_id) == -1)
 		goto fail_bridge;
 
-	goto out;
+	return r;
 
 fail_bridge:
 	bridge_delete(bridge_name);
 	close(r.fd);
 	r.fd = -1;
-out:
-	ch_device_free(router_vm_net_device);
 	return r;
 }

host/start-vmm/net.rs
@@ -8,9 +8,10 @@ use std::os::raw::c_char;
 use miniserde::ser::Fragment;
 use miniserde::Serialize;
 
-use crate::ch::NetConfig;
+use crate::ch::NetConfigC;
 
 #[repr(transparent)]
+#[derive(Copy, Clone)]
 pub struct MacAddress([u8; 6]);
 
 impl MacAddress {
@@ -39,7 +40,7 @@ extern "C" {
     /// # Safety
     ///
     /// `provider_vm_name` must not be null.
-    pub fn net_setup(provider_vm_name: *const c_char) -> NetConfig;
+    pub fn net_setup(provider_vm_name: *const c_char) -> NetConfigC;
 }
 
 #[cfg(test)]