shymega/spectrum-os

commit c3fbeca89c23270314d1a24017160ff6fe7cc497

lib/common.mk: init
We can simplify our Makefiles a bit by moving some things into a
library file.  So far, I've done this for non-component-specific
macros, and inference rules.

Some tar2ext4 rules used a temporary file for the tar2ext4 invocation,
and then moved the file into place when it was done, to prevent a
half-built image sticking around if tar2ext4 died.  According to
POSIX, make should clean up any targets that didn't finish building if
it's interrupted, so we should only end up with a half-built image if
tar2ext4 crashes, e.g. due to running out of disk space.  I think we
should just ignore that possibility, since otherwise we'd have to
write annoying make rules using temporary file names for every target,
and if it does happen recovery is just a make clean away.

With this, I've also made a small reorganisation.  I got rid of the
"nix" directory and created a "lib" directory, where I put
eval-config.nix and the new common.mk file, since both are library
code.  checks.nix isn't really a library though, so I moved it into
the "release" directory, since the only reference to it is in
release.nix.

Signed-off-by: Alyssa Ross <hi@alyssa.is>

host/initramfs/Makefile
@@ -1,16 +1,7 @@
 # SPDX-License-Identifier: EUPL-1.2+
 # SPDX-FileCopyrightText: 2021-2022 Alyssa Ross <hi@alyssa.is>
 
-# qemu-kvm is non-standard, but is present in at least Fedora and
-# Nixpkgs.  If you don't have qemu-kvm, you'll need to set e.g.
-# QEMU_KVM = qemu-system-x86_64 -enable-kvm.
-QEMU_KVM = qemu-kvm
-
-CPIO = cpio
-CPIOFLAGS = --reproducible -R +0:+0 -H newc
-TAR = tar
-TAR2EXT4 = tar2ext4
-VERITYSETUP = veritysetup
+include ../../lib/common.mk
 
 RUN_IMAGE = build/live.img
 
@@ -64,10 +55,6 @@ build/live.img: ../../scripts/format-uuid.sh ../../scripts/make-gpt.sh build/roo
 
 build/loop.tar: build/live.img
 	$(TAR) -cf $@ build/live.img
-
-.SUFFIXES: .ext4 .tar
-.tar.ext4:
-	$(TAR2EXT4) -i $< -o $@
 
 build/loop.img: ../../scripts/make-gpt.sh build/loop.ext4
 	../../scripts/make-gpt.sh $@.tmp \

host/rootfs/Makefile
@@ -1,18 +1,7 @@
 # SPDX-License-Identifier: EUPL-1.2+
 # SPDX-FileCopyrightText: 2021-2022 Alyssa Ross <hi@alyssa.is>
 
-# qemu-kvm is non-standard, but is present in at least Fedora and
-# Nixpkgs.  If you don't have qemu-kvm, you'll need to set e.g.
-# QEMU_KVM = qemu-system-x86_64 -enable-kvm.
-QEMU_KVM = qemu-kvm
-
-VERITYSETUP = veritysetup
-
-# tar2ext4 will leave half a filesystem behind if it's interrupted
-# half way through.
-build/rootfs.ext4: build/rootfs.tar
-	tar2ext4 -i build/rootfs.tar -o $@.tmp
-	mv $@.tmp $@
+include ../../lib/common.mk
 
 FILES = \
 	etc/fonts/fonts.conf \
@@ -46,6 +35,8 @@ DIRS = dev etc/s6-linux-init/env ext run proc sys
 LINKS = bin sbin
 
 BUILD_FILES = build/etc/mdev/modalias.sh build/etc/s6-rc
+
+build/rootfs.ext4:
 
 build/empty:
 	mkdir -p $@

img/app/Makefile
@@ -1,11 +1,7 @@
 # SPDX-License-Identifier: EUPL-1.2+
 # SPDX-FileCopyrightText: 2021-2022 Alyssa Ross <hi@alyssa.is>
 
-# qemu-kvm is non-standard, but is present in at least Fedora and
-# Nixpkgs.  If you don't have qemu-kvm, you'll need to set e.g.
-# QEMU_KVM = qemu-system-x86_64 -enable-kvm.
-QEMU_KVM = qemu-kvm
-CLOUD_HYPERVISOR = cloud-hypervisor
+include ../../lib/common.mk
 
 prefix = /usr/local
 imgdir = $(prefix)/img
@@ -32,13 +28,6 @@ build/host/appvm/blk/root.img: ../../scripts/make-gpt.sh ../../scripts/sfdisk-fi
 	mkdir -p $$(dirname $@)
 	../../scripts/make-gpt.sh $@.tmp \
 	    build/rootfs.ext4:4f68bce3-e8cd-4db1-96e7-fbcaf984b709:5460386f-2203-4911-8694-91400125c604:root
-	mv $@.tmp $@
-
-# tar2ext4 will leave half a filesystem behind if it's interrupted
-# half way through.
-build/rootfs.ext4: build/rootfs.tar
-	mkdir -p $$(dirname $@)
-	tar2ext4 -i build/rootfs.tar -o $@.tmp
 	mv $@.tmp $@
 
 VM_FILES = \

lib/common.mk
@@ -1,0 +1,25 @@
+# SPDX-License-Identifier: EUPL-1.2+
+# SPDX-FileCopyrightText: 2021, 2023 Alyssa Ross <hi@alyssa.is>
+
+.POSIX:
+
+# qemu-kvm is non-standard, but is present in at least Fedora and
+# Nixpkgs.  If you don't have qemu-kvm, you'll need to set e.g.
+# QEMU_KVM = qemu-system-x86_64 -enable-kvm.
+QEMU_KVM = qemu-kvm
+
+CLOUD_HYPERVISOR = cloud-hypervisor
+CPIO = cpio
+CPIOFLAGS = --reproducible -R +0:+0 -H newc
+MCOPY = mcopy
+MKFS_FAT = mkfs.fat
+MMD = mmd
+OBJCOPY = objcopy
+TAR = tar
+TAR2EXT4 = tar2ext4
+TRUNCATE = truncate
+VERITYSETUP = veritysetup
+
+.SUFFIXES: .ext4 .tar
+.tar.ext4:
+	$(TAR2EXT4) -i $< -o $@

release/live/Makefile
@@ -1,17 +1,7 @@
 # SPDX-License-Identifier: EUPL-1.2+
 # SPDX-FileCopyrightText: 2021-2022 Alyssa Ross <hi@alyssa.is>
 
-# qemu-kvm is non-standard, but is present in at least Fedora and
-# Nixpkgs.  If you don't have qemu-kvm, you'll need to set e.g.
-# QEMU_KVM = qemu-system-x86_64 -enable-kvm.
-QEMU_KVM = qemu-kvm
-
-MCOPY = mcopy
-MKFS_FAT = mkfs.fat
-MMD = mmd
-OBJCOPY = objcopy
-TRUNCATE = truncate
-VERITYSETUP = veritysetup
+include ../../lib/common.mk
 
 build/live.img: ../../scripts/format-uuid.sh ../../scripts/make-gpt.sh build/boot.fat build/rootfs.verity.superblock build/rootfs.verity.roothash $(ROOT_FS) $(EXT_FS)
 	../../scripts/make-gpt.sh $@.tmp \

vm/sys/net/Makefile
@@ -1,11 +1,7 @@
 # SPDX-License-Identifier: EUPL-1.2+
 # SPDX-FileCopyrightText: 2021-2022 Alyssa Ross <hi@alyssa.is>
 
-# qemu-kvm is non-standard, but is present in at least Fedora and
-# Nixpkgs.  If you don't have qemu-kvm, you'll need to set e.g.
-# QEMU_KVM = qemu-system-x86_64 -enable-kvm.
-QEMU_KVM = qemu-kvm
-CLOUD_HYPERVISOR = cloud-hypervisor
+include ../../../lib/common.mk
 
 VMM = qemu
 
@@ -34,13 +30,6 @@ build/host/data/netvm/blk/root.img: ../../../scripts/make-gpt.sh ../../../script
 	mkdir -p $$(dirname $@)
 	../../../scripts/make-gpt.sh $@.tmp \
 	    build/rootfs.ext4:4f68bce3-e8cd-4db1-96e7-fbcaf984b709:ea21da27-0391-48da-9235-9d2ab2ca7844:root
-	mv $@.tmp $@
-
-# tar2ext4 will leave half a filesystem behind if it's interrupted
-# half way through.
-build/rootfs.ext4: build/rootfs.tar
-	mkdir -p $$(dirname $@)
-	tar2ext4 -i build/rootfs.tar -o $@.tmp
 	mv $@.tmp $@
 
 VM_FILES = \

Documentation/default.nix
@@ -2,7 +2,7 @@
 # SPDX-FileCopyrightText: 2022 Unikie
 # SPDX-License-Identifier: MIT
 
-import ../nix/eval-config.nix ({ config, src, ... }: config.pkgs.callPackage (
+import ../lib/eval-config.nix ({ config, src, ... }: config.pkgs.callPackage (
 
 { lib, stdenvNoCC, jekyll, drawio-headless }:
 

Documentation/jekyll.nix
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2022 Alyssa Ross <hi@alyssa.is>
 # SPDX-License-Identifier: MIT
 
-import ../nix/eval-config.nix ({ config, ... }: config.pkgs.callPackage (
+import ../lib/eval-config.nix ({ config, ... }: config.pkgs.callPackage (
 
 { bundlerApp }:
 

commit 97ec10f27635d173bf85cc74f888057fa7118b62

lib/common.mk: move .POSIX out
The standard says the following, so I don't think it's specified that
it's allowed to be in an include file:

> If it appears as the first non-comment line in the makefile, make
> shall process the makefile as specified by this section; otherwise,
> the behavior of make is unspecified.

Link: https://pubs.opengroup.org/onlinepubs/9799919799/utilities/make.html#tag_20_76_13_04
Fixes: c3fbeca ("lib/common.mk: init")
Reviewed-by: Dom Rodriguez <shymega@shymega.org.uk>
Signed-off-by: Alyssa Ross <hi@alyssa.is>
Message-ID: <20240908144749.842619-1-hi@alyssa.is>

host/initramfs/Makefile
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: EUPL-1.2+
 # SPDX-FileCopyrightText: 2021-2023 Alyssa Ross <hi@alyssa.is>
 
+.POSIX:
+
 include ../../lib/common.mk
 
 dest = build/initramfs

host/rootfs/Makefile
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: EUPL-1.2+
 # SPDX-FileCopyrightText: 2021-2024 Alyssa Ross <hi@alyssa.is>
 
+.POSIX:
+
 include ../../lib/common.mk
 
 dest = build/rootfs.erofs

img/app/Makefile
@@ -2,6 +2,8 @@
 # SPDX-FileCopyrightText: 2021-2024 Alyssa Ross <hi@alyssa.is>
 # SPDX-FileCopyrightText: 2022 Unikie
 
+.POSIX:
+
 include ../../lib/common.mk
 
 prefix = build/host

lib/common.mk
@@ -1,8 +1,6 @@
 # SPDX-License-Identifier: EUPL-1.2+
 # SPDX-FileCopyrightText: 2021, 2023 Alyssa Ross <hi@alyssa.is>
 
-.POSIX:
-
 # qemu-kvm is non-standard, but is present in at least Fedora and
 # Nixpkgs.  If you don't have qemu-kvm, you'll need to set e.g.
 # QEMU_KVM = qemu-system-x86_64 -enable-kvm.

release/live/Makefile
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: EUPL-1.2+
 # SPDX-FileCopyrightText: 2021-2023 Alyssa Ross <hi@alyssa.is>
 
+.POSIX:
+
 include ../../lib/common.mk
 
 dest = build/live.img

vm/sys/net/Makefile
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: EUPL-1.2+
 # SPDX-FileCopyrightText: 2021-2024 Alyssa Ross <hi@alyssa.is>
 
+.POSIX:
+
 include ../../../lib/common.mk
 
 prefix = build/svc