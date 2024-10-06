shymega/spectrum-os

commit 35c8269325c3d03c0592a25abb9a469bd41697e7

vm-lib/make-vm.nix: remove indirection
There's no longer any need to construct a synthentic root in this
script, since make-erofs.sh accepts source/dest pairs.

Signed-off-by: Alyssa Ross <hi@alyssa.is>

vm-lib/make-vm.nix
@@ -32,11 +32,7 @@ runCommand "spectrum-vm" {
 } ''
   mkdir -p "$out"/{blk,providers,shared-dirs}
 
-  mkdir root
-  cd root
-  ln -s ${run} run
-
-  ${../scripts/make-erofs.sh} -L ext -- "$out/blk/run.img" run run \
+  ${../scripts/make-erofs.sh} -L ext -- "$out/blk/run.img" ${run} run \
       $(comm -23 <(sort ${writeReferencesToFile run}) \
           <(sort ${writeReferencesToFile basePaths}) | sed p)
 

commit 460496a582fc69d6602be828f724e94815381610

vm-lib/make-vm.nix: don't copy run
This is a Nix store path, and it will be included in the closure we
copy, so it should be a symlink into the store rather than a redundant
copy.

Fixes: 35c8269 ("vm-lib/make-vm.nix: remove indirection")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

vm-lib/make-vm.nix
@@ -33,8 +33,9 @@ runCommand "spectrum-vm" {
 } ''
   mkdir -p "$out"/{blk,providers}
 
+  ln -s ${run} run
   (
-      printf "%s\nrun\n" ${run}
+      printf "run\nrun\n"
       comm -23 <(sort ${writeClosure [ run ]}) \
           <(sort ${writeClosure [ basePaths ]}) | sed p
   ) | ${../scripts/make-erofs.sh} -L ext "$out/blk/run.img"