shymega/spectrum-os

commit f76b542bf129def2dbca7c99ef1cc2ec6386dd2f

release/checks/wayland: init
This is a smoke test for crosvm vhost-user-gpu.  It starts Weston and
the img/app VM in crosvm, and checks that the hello-wayland window
from the VM appears, using a small Weston plugin.

Hopefully this will make it easier to keep up with upstream crosvm,
because testing crosvm versions can be automated.

Signed-off-by: Alyssa Ross <hi@alyssa.is>

release/checks/wayland/surface-notify/module.c
@@ -1,0 +1,79 @@
+// SPDX-License-Identifier: EUPL-1.2+
+// SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
+
+// A Weston module that sends a notification into a fifo when a
+// hello-wayland window appears.
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <libweston/libweston.h>
+#include <libweston/zalloc.h>
+
+static void on_commit(struct wl_listener *, struct weston_surface *surface)
+{
+	int fd;
+	int32_t width, height;
+
+	// Use the size of the surface as a heuristic for identifying
+	// hello-wayland.  If we had security contexts[1], we could be
+	// more precise about this.
+	//
+	// [1]: https://gitlab.freedesktop.org/wayland/wayland-protocols/-/merge_requests/68
+	weston_surface_get_content_size(surface, &width, &height);
+	if (!surface->output || width != 128 || height != 128)
+		return;
+
+	if ((fd = open("/run/surface-notify", O_WRONLY)) == -1) {
+		weston_log("opening /run/surface-notify: %m\n");
+		return;
+	}
+
+	if (write(fd, "\n", 1) == -1)
+		weston_log("writing to /run/surface-notify: %m\n");
+
+	close(fd);
+}
+
+static void on_destroy(struct wl_listener *listener,
+		       struct weston_surface *surface)
+{
+	struct wl_listener *commit_listener = wl_signal_get(
+		&surface->commit_signal, (wl_notify_func_t)on_commit);
+
+	wl_list_remove(&commit_listener->link);
+	free(commit_listener);
+
+	wl_list_remove(&listener->link);
+	free(listener);
+}
+
+static void add_listener(struct wl_signal *signal, wl_notify_func_t notify)
+{
+	struct wl_listener *listener = zalloc(sizeof *listener);
+	if (!listener) {
+		weston_log("failed to allocate listener\n");
+		return;
+	}
+
+	listener->notify = notify;
+	wl_signal_add(signal, listener);
+}
+
+static void on_create(struct wl_listener *, struct weston_surface *surface)
+{
+	add_listener(&surface->commit_signal, (wl_notify_func_t)on_commit);
+	add_listener(&surface->destroy_signal, (wl_notify_func_t)on_destroy);
+}
+
+static struct wl_listener create_surface_listener = {
+	.notify = (wl_notify_func_t)on_create,
+};
+
+int wet_module_init(struct weston_compositor *compositor, int *, char *[])
+{
+	wl_signal_add(&compositor->create_surface_signal,
+		      &create_surface_listener);
+
+	return 0;
+}

host/rootfs/default.nix
@@ -136,7 +136,7 @@ stdenvNoCC.mkDerivation {
 
   enableParallelBuilding = true;
 
-  passthru = { inherit firmware kernel nixosAllHardware; };
+  passthru = { inherit appvm firmware kernel nixosAllHardware; };
 
   meta = with lib; {
     license = licenses.eupl12;

release/checks/default.nix
@@ -21,4 +21,6 @@ import ../../lib/eval-config.nix ({ ... } @ args:
   shellcheck = import ./shellcheck.nix args;
 
   try = import ./try.nix args;
+
+  wayland = import ./wayland args;
 })

release/checks/wayland/default.nix
@@ -1,0 +1,70 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
+
+import ../../../lib/eval-config.nix ({ config, ... }:
+
+let
+  inherit (import ../../../host/rootfs { inherit config; }) appvm;
+  run = import ../../../vm/app/hello-wayland.nix { inherit config; };
+  surface-notify = import ./surface-notify { inherit config; };
+in
+
+config.pkgs.nixosTest ({ pkgs, ... }: {
+  name = "spectrum-wayland";
+
+  nodes.machine = { ... }: {
+    hardware.opengl.enable = true;
+
+    systemd.services.crosvm = {
+      after = [ "crosvm-gpu.service" "weston.service" ];
+      requires = [ "crosvm-gpu.service" "weston.service" ];
+      serviceConfig.ExecStart = "${pkgs.crosvm}/bin/crosvm run --disk ${appvm}/img/appvm/blk/root.img --disk ${run}/blk/run.img -p \"console=ttyS0 root=PARTLABEL=root\" --vhost-user-gpu /run/crosvm-gpu.sock --serial type=stdout,hardware=virtio-console,stdin=true ${appvm}/img/appvm/vmlinux";
+    };
+
+    systemd.services.crosvm-gpu = {
+      requires = [ "weston.service" ];
+      script = ''
+        rm -f /run/crosvm-gpu.sock
+        (
+            while ! [ -S /run/crosvm-gpu.sock ]; do
+                sleep .1
+            done
+            systemd-notify --ready --no-block
+        ) &
+        exec ${pkgs.crosvm}/bin/crosvm device gpu \
+            --socket /run/crosvm-gpu.sock \
+            --wayland-sock /run/wayland-1 \
+            --params '{"context-types":"cross-domain"}'
+      '';
+      serviceConfig.NotifyAccess = "all";
+      serviceConfig.Type = "notify";
+    };
+
+    systemd.services.surface-notify-socket = {
+      serviceConfig.ExecStart = "${pkgs.coreutils}/bin/mkfifo /run/surface-notify";
+      serviceConfig.RemainAfterExit = true;
+      serviceConfig.Type = "oneshot";
+    };
+
+    systemd.services.weston = {
+      after = [ "surface-notify-socket.service" ];
+      requires = [ "surface-notify-socket.service" ];
+      environment.XDG_RUNTIME_DIR = "/run";
+      environment.WAYLAND_DEBUG = "server";
+      serviceConfig.ExecStart = "${pkgs.westonLite}/bin/weston --modules ${surface-notify}/lib/weston/surface-notify.so,systemd-notify.so";
+      serviceConfig.TTYPath = "/dev/tty7";
+      serviceConfig.Type = "notify";
+    };
+  };
+
+  testScript = { ... }: ''
+    machine.wait_for_unit('multi-user.target')
+
+    machine.start_job('crosvm.service')
+
+    machine.wait_for_unit('surface-notify-socket.service');
+    machine.succeed('test "$(wc -c /run/surface-notify)" = "1 /run/surface-notify"', timeout=180)
+
+    machine.screenshot('weston')
+  '';
+}))

release/checks/wayland/surface-notify/default.nix
@@ -1,0 +1,19 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
+
+import ../../../../lib/eval-config.nix ({ config, src, ... }: config.pkgs.callPackage (
+
+{ lib, stdenv, meson, ninja, pkg-config
+, libxkbcommon, pixman, wayland, westonLite
+}:
+
+stdenv.mkDerivation {
+  name = "surface-notify";
+
+  inherit src;
+  sourceRoot = "source/release/checks/wayland/surface-notify";
+
+  nativeBuildInputs = [ meson ninja pkg-config ];
+  buildInputs = [ libxkbcommon pixman wayland westonLite ];
+}
+) { })

release/checks/wayland/surface-notify/meson.build
@@ -1,0 +1,13 @@
+# SPDX-License-Identifier: EUPL-1.2+
+# SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
+
+project('surface-notify', 'c', default_options : [ 'warning_level=2' ])
+
+libweston = dependency('libweston-12')
+wayland_server = dependency('wayland-server')
+
+shared_library('surface-notify', 'module.c',
+  name_prefix : '',
+  dependencies : [ libweston, wayland_server ],
+  install : true,
+  install_dir : get_option('libdir') / 'weston')

release/checks/wayland/surface-notify/shell.nix
@@ -1,0 +1,9 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
+
+{ ... } @ args:
+
+(import ./. args).overrideAttrs ({ hardeningDisable ? [], ... }: {
+  # Not compatible with Meson's default -O0.
+  hardeningDisable = hardeningDisable ++ [ "fortify" ];
+}))

commit ab3a774514a87d41bab9c665850c9ee9f1927382

Format C files with Uncrustify
Fixes: 338f3ad ("host/start-vm: add tests for bridge functions")
Fixes: f76b542 ("release/checks/wayland: init")
Fixes: a2d362c ("host: allow VMs to be powered off")
Fixes: 70e8858 ("host/start-vmm: mark non-NULL function parameters")
Fixes: 4d59577 ("host/start-vmm: fix cleaning up when VMM exits")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

host/start-vmm/fork.c
@@ -28,7 +28,7 @@ int double_fork(void)
 	case 0:
 		close(fd[0]);
 		switch ((v = fork())) {
-	        case 0:
+		case 0:
 			close(fd[1]);
 			return 0;
 		case -1:

host/start-vmm/net.c
@@ -19,8 +19,8 @@
 #include <linux/if_tun.h>
 
 static int setup_tap(const char bridge_name[static 1],
-		     const char tap_prefix[static 1],
-		     char tap_name[static IFNAMSIZ])
+                     const char tap_prefix[static 1],
+                     char tap_name[static IFNAMSIZ])
 {
 	int fd;
 
@@ -43,16 +43,16 @@ out:
 }
 
 static int client_net_setup(const char bridge_name[static 1],
-			    char tap_name[static IFNAMSIZ])
+                            char tap_name[static IFNAMSIZ])
 {
 	return setup_tap(bridge_name, "client", tap_name);
 }
 
 [[gnu::nonnull]]
 static int router_net_setup(const char bridge_name[static 1],
-			    const struct vm_dir *router_vm_dir,
+                            const struct vm_dir *router_vm_dir,
                             const uint8_t mac[6],
-			    char device_id_out[static IFNAMSIZ])
+                            char device_id_out[static IFNAMSIZ])
 {
 	struct net_config net;
 	int e;

release/checks/wayland/surface-notify/module.c
@@ -35,7 +35,7 @@ static void on_commit(struct wl_listener *, struct weston_surface *surface)
 }
 
 static void on_destroy(struct wl_listener *listener,
-		       struct weston_surface *surface)
+                       struct weston_surface *surface)
 {
 	struct wl_listener *commit_listener = wl_signal_get(
 		&surface->commit_signal, (wl_notify_func_t)on_commit);
@@ -72,7 +72,7 @@ static struct wl_listener create_surface_listener = {
 int wet_module_init(struct weston_compositor *compositor, int *, char *[])
 {
 	wl_signal_add(&compositor->create_surface_signal,
-		      &create_surface_listener);
+	              &create_surface_listener);
 
 	return 0;
 }

host/start-vmm/tests/bridge_add_if.c
@@ -35,8 +35,8 @@ int main(void)
 	assert(bridge_add_if(bridge_name, tap_name) != -1);
 
 	r = snprintf(brif_path, sizeof brif_path,
-		     "/sys/devices/virtual/net/%s/brif/%s",
-		     bridge_name, tap_name);
+	             "/sys/devices/virtual/net/%s/brif/%s",
+	             bridge_name, tap_name);
 	assert(r > 0 && (size_t)r < sizeof brif_path);
 
 	assert(!lstat(brif_path, &statbuf));

host/start-vmm/tests/bridge_remove_if.c
@@ -36,8 +36,8 @@ int main(void)
 	assert(bridge_remove_if(bridge_name, tap_name) != -1);
 
 	r = snprintf(brif_path, sizeof brif_path,
-		     "/sys/devices/virtual/net/%s/brif/%s",
-		     bridge_name, tap_name);
+	             "/sys/devices/virtual/net/%s/brif/%s",
+	             bridge_name, tap_name);
 	assert(r > 0 && (size_t)r < sizeof brif_path);
 
 	assert(lstat(brif_path, &statbuf) == -1);