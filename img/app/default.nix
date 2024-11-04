shymega/spectrum-os

commit 57a8921790aa4fd1d11c308d29991d5a18102730

host/rootfs: always share a single dir with VMs
This makes everything simpler.  Having multiple virtio-fs devices
would be inefficient, and would raise the question of how VMs should
identify which one is which, and it doesn't seem to buy us anything
over bind mounts into a single directory.

As part of this change, I've removed the static configuration of
shared directories rather than adapting it.  It would be possible to
implement it, but I think that (like much of the rest of Spectrum)
we'll be able to better design static configuration if we get dynamic
stuff working first.

Currently, VMs get this single shared directory whether they want it
or not.  We could make it optional in future.

mount now comes from util-linux, because if you do mount --bind -o ro
with busybox mount, it'll just ignore the -o ro part.  Terrifying.

Signed-off-by: Alyssa Ross <hi@alyssa.is>

vm-lib/make-vm.nix
@@ -33,7 +33,7 @@ runCommand "spectrum-vm" {
     (mapAttrsToList (kind: map (vm: "${kind}/${vm}\n")) providers));
   passAsFile = [ "providerDirs" ];
 } ''
-  mkdir -p "$out"/{blk,providers,shared-dirs}
+  mkdir -p "$out"/{blk,providers}
 
   (
       printf "%s\nrun\n" ${run}
@@ -46,13 +46,6 @@ runCommand "spectrum-vm" {
   pushd providers
   xargs -rd '\n' dirname -- < "$providerDirsPath" | xargs -rd '\n' mkdir -p --
   xargs -rd '\n' touch -- < "$providerDirsPath"
-  popd
-
-  pushd shared-dirs
-  ${concatStringsSep "\n" (mapAttrsToList (key: { path }: ''
-    mkdir ${lib.escapeShellArg key}
-    ln -s ${lib.escapeShellArgs [ path "${key}/dir" ]}
-  '') sharedDirs)}
   popd
 
   popd

host/rootfs/default.nix
@@ -67,6 +67,7 @@ let
         CONFIG_MKFS_EXT2 n
         CONFIG_MODINFO n
         CONFIG_MODPROBE n
+        CONFIG_MOUNT n
         CONFIG_RMMOD n
       '';
     })
@@ -101,7 +102,7 @@ let
     # TODO: this is a hack and we should just build the util-linux
     # programs we want.
     # https://lore.kernel.org/util-linux/87zgrl6ufb.fsf@alyssa.is/
-    ln -s ${util-linuxMinimal}/bin/{findfs,lsblk} $out/usr/bin
+    ln -s ${util-linuxMinimal}/bin/{findfs,lsblk,mount} $out/usr/bin
   '';
 in
 

host/rootfs/Makefile
@@ -38,7 +38,6 @@ FILES = \
 	etc/s6-linux-init/run-image/service/vmm/run \
 	etc/s6-linux-init/run-image/service/vmm/template/notification-fd \
 	etc/s6-linux-init/scripts/rc.init \
-	etc/shared-dir \
 	etc/xdg/weston/autolaunch \
 	etc/xdg/weston/weston.ini \
 	usr/bin/lsvm \

host/rootfs/etc/s6-linux-init/run-image/service/vhost-user-fs/template/run
@@ -1,6 +1,6 @@
 #!/bin/execlineb -S1
 # SPDX-License-Identifier: EUPL-1.2+
-# SPDX-FileCopyrightText: 2021-2023 Alyssa Ross <hi@alyssa.is>
+# SPDX-FileCopyrightText: 2021-2024 Alyssa Ross <hi@alyssa.is>
 
 s6-ipcserver-socketbinder -a 0700 -B env/virtiofsd.sock
 
@@ -8,5 +8,10 @@ if { fdmove 1 3 echo }
 fdclose 3
 
 export TMPDIR /run
-backtick -E shared_dir { /etc/shared-dir -v servicename=${1} }
-virtiofsd --fd 0 --shared-dir $shared_dir
+
+# The VM should not be able to write directly into a tmpfs, but there
+# can be writable block-based bind mounted subdirectories.
+unshare -m --propagation slave
+if { mount --rbind -o ro /run/vm/${1}/fs /run/vm/${1}/fs }
+
+virtiofsd --fd 0 --shared-dir /run/vm/${1}/fs

host/rootfs/etc/s6-rc/ext-rc-init/up
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: EUPL-1.2+
-# SPDX-FileCopyrightText: 2021-2023 Alyssa Ross <hi@alyssa.is>
+# SPDX-FileCopyrightText: 2021-2024 Alyssa Ross <hi@alyssa.is>
 # SPDX-FileCopyrightText: 2022 Unikie
 
 cd /ext/svc/data
@@ -11,17 +11,9 @@ if {
   if { mkdir /run/vm/${name} }
   if { ln -s /ext/svc/data/${name} /run/vm/${name}/config }
 
-  if {
-    if -t { test -e ${name}/shared-dirs }
-    cd ${name}/shared-dirs
-    elglob -0 fsnames *
-
-    if {
-      forx -po0 -E fsname { $fsnames }
-      s6-instance-create /run/service/vhost-user-fs ${name}:${fsname}
-    }
-    s6-svwait -U /run/service/vhost-user-fs/instance/${name}:${fsnames}
-  }
+  if { mkdir /run/vm/${name}/fs }
+  if { s6-instance-create /run/service/vhost-user-fs $name }
+  if { s6-svwait -U /run/service/vhost-user-fs/instance/${name} }
 
   if {
     if -t { test -e ${name}/wayland }

host/rootfs/etc/shared-dir
@@ -1,10 +1,0 @@
-#!/bin/awk -f
-# SPDX-License-Identifier: EUPL-1.2+
-# SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
-
-# Usage: /etc/shared-dir -v servicename=[servicename]
-
-BEGIN {
-	split(servicename, names, ":")
-	print("/ext/svc/data/" names[1] "/shared-dirs/" names[2] "/dir")
-}

host/start-vmm/ch.rs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: EUPL-1.2+
-// SPDX-FileCopyrightText: 2022-2023 Alyssa Ross <hi@alyssa.is>
+// SPDX-FileCopyrightText: 2022-2024 Alyssa Ross <hi@alyssa.is>
 
 use std::ffi::{CStr, OsStr, OsString};
 use std::io::Write;
@@ -33,7 +33,7 @@ pub struct DiskConfig {
 #[derive(Serialize)]
 pub struct FsConfig {
     pub socket: String,
-    pub tag: String,
+    pub tag: &'static str,
 }
 
 #[derive(Serialize)]
@@ -64,7 +64,7 @@ pub struct PayloadConfig {
 pub struct VmConfig {
     pub console: ConsoleConfig,
     pub disks: Vec<DiskConfig>,
-    pub fs: Vec<FsConfig>,
+    pub fs: [FsConfig; 1],
     pub gpu: Vec<GpuConfig>,
     pub memory: MemoryConfig,
     pub net: Vec<NetConfig>,

host/start-vmm/lib.rs
@@ -65,7 +65,6 @@ pub fn vm_config(vm_name: &str, config_root: &Path) -> Result<VmConfig, String> 
     let blk_dir = config_dir.join("blk");
     let kernel_path = config_dir.join("vmlinux");
     let net_providers_dir = config_dir.join("providers/net");
-    let shared_dirs_dir = config_dir.join("shared-dirs");
     let wayland_path = config_dir.join("wayland");
 
     Ok(VmConfig {
@@ -102,27 +101,10 @@ pub fn vm_config(vm_name: &str, config_root: &Path) -> Result<VmConfig, String> 
                 .collect::<Result<_, _>>()?,
             Err(e) => return Err(format!("reading directory {:?}: {}", blk_dir, e)),
         },
-        fs: match shared_dirs_dir.read_dir() {
-            Ok(entries) => entries
-                .into_iter()
-                .map(|result| {
-                    let entry = result
-                        .map_err(|e| format!("examining directory entry: {}", e))?
-                        .file_name();
-
-                    let entry = entry.to_str().ok_or_else(|| {
-                        format!("shared directory name {:?} is not valid UTF-8", entry)
-                    })?;
-
-                    Ok(FsConfig {
-                        tag: entry.to_string(),
-                        socket: format!("/run/service/vhost-user-fs/instance/{vm_name}:{entry}/env/virtiofsd.sock"),
-                    })
-                })
-                .collect::<Result<_, String>>()?,
-            Err(e) if e.kind() == ErrorKind::NotFound => Default::default(),
-            Err(e) => return Err(format!("reading directory {:?}: {e}", shared_dirs_dir)),
-        },
+        fs: [FsConfig {
+            tag: "virtiofs0",
+            socket: format!("/run/service/vhost-user-fs/instance/{vm_name}/env/virtiofsd.sock"),
+        }],
         gpu: match wayland_path.try_exists() {
             Ok(true) => vec![GpuConfig {
                 socket: format!("/run/service/vhost-user-gpu/instance/{vm_name}/env/crosvm.sock"),

commit c0bca84e97be31b1fe3108f71564384d63428c48

Remove unused code identified by deadnix
Fixes: 57a8921 ("host/rootfs: always share a single dir with VMs")
Fixes: c92550d ("img/app: install libs, bins needed for AppImages")
Fixes: 0f07afb ("img/app: get application config from virtio-fs")
Fixes: 296ccff ("Build images with __structuredAttrs")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

img/app/default.nix
@@ -53,8 +53,6 @@ let
 in
 
 let
-  inherit (lib) concatMapStringsSep;
-
   packagesSysroot = runCommand "packages-sysroot" {} ''
     mkdir -p $out/etc/ssl/certs
     ln -s ${appimageFhsenv}/{lib64,usr} ${kernel}/lib $out

img/app/shell.nix
@@ -10,7 +10,7 @@ import ../../lib/call-package.nix (
 }:
 
 (callSpectrumPackage ./. {}).overrideAttrs (
-{ nativeBuildInputs ? [], shellHook ? "", passthru ? {}, ... }:
+{ nativeBuildInputs ? [], passthru ? {}, ... }:
 
 {
   nativeBuildInputs = nativeBuildInputs ++ [

vm-lib/make-vm.nix
@@ -17,9 +17,7 @@ pkgs.pkgsStatic.callPackage (
 { run, type, providers ? {}, sharedDirs ? {} }:
 
 let
-  inherit (lib)
-    any attrValues concatLists concatStrings concatStringsSep hasInfix
-    mapAttrsToList;
+  inherit (lib) any attrValues concatLists hasInfix mapAttrsToList;
 in
 
 assert !(any (hasInfix "\n") (concatLists (attrValues providers)));