shymega/spectrum-os

commit 63c67f1d1181342a62cd3cb12ccb154f9a533ee1

host/start-vmm: use a Meson option for panic
This is a much more meson-y way to handle this, and it should make it
easier to use Meson's new Cargo wraps.

I don't like that it's the non-native dependencies that need to have
this option, but it seems to fit into Meson much better this way, so
it is what it is.

Signed-off-by: Alyssa Ross <hi@alyssa.is>

host/start-vmm/default.nix
@@ -69,9 +69,7 @@ stdenv.mkDerivation (finalAttrs: {
     ln -s ${crate} subprojects/packagecache/${crate.name}
   '') packageCache;
 
-  preConfigure = ''
-    mesonFlagsArray+=(-Drust_args="-C panic=abort" -Dtests=false -Dwerror=true)
-  '';
+  mesonFlags = [ "-Dtests=false" "-Dunwind=false" "-Dwerror=true" ];
 
   passthru.tests = {
     clippy = finalAttrs.finalPackage.overrideAttrs (
@@ -85,7 +83,6 @@ stdenv.mkDerivation (finalAttrs: {
           # non-subprojects without enumerating the subprojects.
           # https://github.com/mesonbuild/meson/issues/9398#issuecomment-954094750
           mesonFlagsArray+=(
-              -Dwerror=true
               -Dproc-macro2:werror=false
               -Dproc-macro2:warning_level=0
           )

host/start-vmm/meson.build
@@ -1,10 +1,14 @@
 # SPDX-License-Identifier: EUPL-1.2+
-# SPDX-FileCopyrightText: 2022-2023 Alyssa Ross <hi@alyssa.is>
+# SPDX-FileCopyrightText: 2022-2024 Alyssa Ross <hi@alyssa.is>
 
 project('start-vmm', 'rust', 'c',
   default_options : ['c_std=c2x', 'rust_std=2018'])
 
 add_project_arguments('-D_GNU_SOURCE', '-Wno-error=attributes', language : 'c')
+
+if not get_option('unwind')
+  add_project_arguments('-C', 'panic=abort', language : 'rust')
+endif
 
 miniserde_dep = dependency('miniserde-rs')
 

host/start-vmm/subprojects/packagefiles/itoa/meson.build
@@ -1,7 +1,11 @@
-# SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
+# SPDX-FileCopyrightText: 2023-2024 Alyssa Ross <hi@alyssa.is>
 # SPDX-License-Identifier: MIT
 
 project('itoa', 'rust', version : '1.0.10', default_options : ['rust_std=2018'])
+
+if not get_option('unwind')
+  add_project_arguments('-C', 'panic=abort', language : 'rust')
+endif
 
 itoa = static_library('itoa', 'src/lib.rs', rust_crate_type : 'rlib')
 

host/start-vmm/subprojects/packagefiles/miniserde/meson.build
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
+# SPDX-FileCopyrightText: 2023-2024 Alyssa Ross <hi@alyssa.is>
 # SPDX-License-Identifier: MIT
 
 project('miniserde', 'rust', version : '0.1.38',
@@ -10,15 +10,20 @@ syn_dep = dependency('syn-rs', native : true)
 mini_internal = shared_library('mini_internal', 'derive/src/lib.rs',
   dependencies : [quote_dep, syn_dep],
   native : true,
-  rust_args : ['-C', 'panic=unwind'],
   rust_crate_type : 'proc-macro')
 
 itoa_dep = dependency('itoa-rs')
 ryu_dep = dependency('ryu-rs')
 
+miniserde_rust_args = []
+if not get_option('unwind')
+  miniserde_rust_args += ['-C', 'panic=abort']
+endif
+
 miniserde = static_library('miniserde', 'src/lib.rs',
   dependencies : [itoa_dep, ryu_dep],
   link_with : mini_internal,
+  rust_args : miniserde_rust_args,
   rust_crate_type : 'rlib')
 
 miniserde_dep = declare_dependency(

host/start-vmm/subprojects/packagefiles/proc-macro2/meson.build
@@ -9,7 +9,7 @@ unicode_ident_dep = dependency('unicode-ident-rs', native : true)
 proc_macro2 = static_library('proc_macro2', 'src/lib.rs',
   dependencies : unicode_ident_dep,
   native : true,
-  rust_args : ['-C', 'panic=unwind', '--cfg', 'feature="proc-macro"'],
+  rust_args : ['--cfg', 'feature="proc-macro"'],
   rust_crate_type : 'rlib')
 
 proc_macro2_dep = declare_dependency(

host/start-vmm/subprojects/packagefiles/quote/meson.build
@@ -9,7 +9,6 @@ proc_macro2_dep = dependency('proc-macro2-rs', native : true)
 quote = static_library('quote', 'src/lib.rs',
   dependencies : proc_macro2_dep,
   native : true,
-  rust_args : ['-C', 'panic=unwind'],
   rust_crate_type : 'rlib')
 
 quote_dep = declare_dependency(

host/start-vmm/subprojects/packagefiles/ryu/meson.build
@@ -1,7 +1,11 @@
-# SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
+# SPDX-FileCopyrightText: 2023-2024 Alyssa Ross <hi@alyssa.is>
 # SPDX-License-Identifier: MIT
 
 project('ryu', 'rust', version : '1.0.17', default_options : ['rust_std=2018'])
+
+if not get_option('unwind')
+  add_project_arguments('-C', 'panic=abort', language : 'rust')
+endif
 
 ryu = static_library('ryu', 'src/lib.rs', rust_crate_type : 'rlib')
 

host/start-vmm/subprojects/packagefiles/syn/meson.build
@@ -11,7 +11,6 @@ syn = static_library('syn', 'src/lib.rs',
   dependencies : [proc_macro2_dep, quote_dep],
   native : true,
   rust_args : [
-    '-C', 'panic=unwind',
     '--cfg', 'feature="clone-impls"',
     '--cfg', 'feature="derive"',
     '--cfg', 'feature="parsing"',

commit 345aae721e275e21242ed6b2a51867b059a7f99c

start-vmm.tests.tests: fix running tests
The preConfigure override used to clear panic=abort and -Dtests=false,
but it no longer does.  This meant that the tests were no longer being
run.

Fixes: 63c67f1 ("host/start-vmm: use a Meson option for panic")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

host/start-vmm/default.nix
@@ -114,11 +114,19 @@ stdenv.mkDerivation (finalAttrs: {
 
     run = run-spectrum-vm.override { start-vmm = finalAttrs.finalPackage; };
 
-    tests = finalAttrs.finalPackage.overrideAttrs ({ name, ... }: {
-      name = "${name}-tests";
-      preConfigure = "";
-      doCheck = true;
-    });
+    tests = finalAttrs.finalPackage.overrideAttrs (
+      { name, mesonFlags ? [], ... }:
+      {
+        name = "${name}-tests";
+
+        mesonFlags = mesonFlags ++ [
+          "-Dunwind=true"
+          "-Dtests=true"
+        ];
+
+        doCheck = true;
+      }
+    );
   };
 
   meta = {