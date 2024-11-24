shymega/spectrum-os

commit 03174be6db00a95a059ba7cbd1ca6dcd56c24051

tools/xdg-desktop-portal-spectrum: init
Signed-off-by: Alyssa Ross <hi@alyssa.is>

tools/xdg-desktop-portal-spectrum/xdg-desktop-portal-spectrum.c
@@ -1,0 +1,149 @@
+// SPDX-License-Identifier: EUPL-1.2+
+// SPDX-FileCopyrightText: 2024 Alyssa Ross <hi@alyssa.is>
+
+#include <arpa/inet.h>
+#include <ctype.h>
+#include <err.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <sys/stat.h>
+
+#include <linux/vm_sockets.h>
+
+#include <dbus/dbus.h>
+
+#include "config.h"
+
+static const uint32_t HOST_PORT = 219;
+
+static int parse_u32(const char *s, uint32_t *v)
+{
+	char *end;
+
+	errno = EINVAL;
+	if (!s || !isdigit(s[0]))
+		return -1;
+
+	errno = 0;
+	*v = strtol(s, &end, 10);
+	if (errno)
+		return -1;
+	if (*end) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	return 0;
+}
+
+static int write_all(int fd, const void *buf, size_t len)
+{
+	int r;
+
+	do {
+		r = write(fd, buf, len);
+		if (r == -1)
+			return -1;
+		buf = (char *)buf + r;
+		len -= r;
+	} while (len);
+
+	return 0;
+}
+
+static int connect_to_host(void)
+{
+	struct sockaddr_vm addr = {
+		.svm_family = AF_VSOCK,
+		.svm_cid = VMADDR_CID_HOST,
+		.svm_port = HOST_PORT,
+	};
+	char handshake[] = { 1, 1 };
+	char version;
+	int sock = socket(AF_VSOCK, SOCK_STREAM, 0);
+
+	if (sock == -1)
+		err(EXIT_FAILURE, "creating vsock socket");
+
+	if (connect(sock, (struct sockaddr *)&addr, sizeof addr) == -1)
+		err(EXIT_FAILURE, "connecting to cid %" PRIu32 " port %" PRIu32,
+		    addr.svm_cid, addr.svm_port);
+
+	if (write_all(sock, handshake, sizeof handshake) == -1)
+		err(EXIT_FAILURE, "writing handshake to vsock socket");
+
+	if (read(sock, &version, 1) == -1)
+		err(EXIT_FAILURE, "reading handshake version");
+	if (version != 1)
+		err(EXIT_FAILURE, "unexpected protocol version %d", version);
+
+	return sock;
+}
+
+constexpr size_t HOST_FS_ROOT_DIR_LEN = sizeof HOST_FS_ROOT_DIR - 1;
+static_assert(HOST_FS_ROOT_DIR_LEN < UINT32_MAX);
+
+static void send_info(int sock, uint32_t port)
+{
+	uint32_t fs_root_dir_len_u32_be = htonl(HOST_FS_ROOT_DIR_LEN);
+	port = htonl(port);
+
+	if (write_all(sock, &port, sizeof port) == -1)
+		err(EXIT_FAILURE, "writing port to vsock socket");
+	if (write_all(sock, &fs_root_dir_len_u32_be, 4) == -1)
+		err(EXIT_FAILURE, "writing fs root length to vsock socket");
+	if (write_all(sock, HOST_FS_ROOT_DIR, HOST_FS_ROOT_DIR_LEN) == -1)
+		err(EXIT_FAILURE, "writing fs root to vsock socket");
+}
+
+static void check_result(int sock)
+{
+	char r;
+	if (read(sock, &r, 1) == -1)
+		err(EXIT_FAILURE, "reading result");
+	if (r)
+		err(EXIT_FAILURE, "host send bad result: %hhd", r);
+}
+
+int main(void)
+{
+	char *addr = getenv("DBUS_STARTER_ADDRESS");
+
+	DBusAddressEntry **entries;
+	int entries_len, i, sock;
+	DBusError error;
+
+	const char *port_str;
+	uint32_t port;
+
+	if (!addr)
+		errx(EXIT_FAILURE, "DBUS_STARTER_ADDRESS not set");
+
+	if (!dbus_parse_address(addr, &entries, &entries_len, &error))
+		errx(EXIT_FAILURE, "parsing D-Bus address '%s': %s",
+		     addr, error.message);
+
+	for (i = 0; i < entries_len; i++) {
+		if (strcmp(dbus_address_entry_get_method(entries[i]), "vsock"))
+			continue;
+
+		if (!(port_str = dbus_address_entry_get_value(entries[i], "port")))
+			errx(EXIT_FAILURE, "missing vsock port in D-Bus address '%s'",
+			     addr);
+
+		if (parse_u32(port_str, &port) == -1)
+			err(EXIT_FAILURE, "D-Bus address vsock port");
+
+		sock = connect_to_host();
+		send_info(sock, port);
+		check_result(sock);
+		return 0;
+	}
+}

pkgs/default.nix
@@ -41,6 +41,8 @@ let
     run-spectrum-vm = self.callSpectrumPackage ../scripts/run-spectrum-vm.nix {};
     xdg-desktop-portal-spectrum-host =
       self.callSpectrumPackage ../tools/xdg-desktop-portal-spectrum-host {};
+    xdg-desktop-portal-spectrum =
+      self.callSpectrumPackage ../tools/xdg-desktop-portal-spectrum {};
 
     # Packages from the overlay, so it's possible to build them from
     # the CLI easily.

tools/xdg-desktop-portal-spectrum/default.nix
@@ -1,0 +1,45 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2024 Alyssa Ross <hi@alyssa.is>
+
+import ../../lib/call-package.nix (
+{ src, lib, stdenv, clang-tools, meson, ninja, pkg-config, dbus }:
+
+stdenv.mkDerivation (finalAttrs: {
+  name = "xdg-desktop-portal-spectrum";
+
+  src = lib.fileset.toSource {
+    root = ../..;
+    fileset = lib.fileset.intersection src ./.;
+  };
+  sourceRoot = "source/tools/xdg-desktop-portal-spectrum";
+
+  mesonFlags = [
+    "-Dhostfsrootdir=/run/virtiofs/virtiofs0"
+  ];
+
+  nativeBuildInputs = [ meson ninja pkg-config ];
+  buildInputs = [ dbus ];
+
+  passthru.tests = {
+    clang-tidy = finalAttrs.finalPackage.overrideAttrs (
+      { nativeBuildInputs ? [], ... }:
+      {
+        nativeBuildInputs = nativeBuildInputs ++ [ clang-tools ];
+
+        buildPhase = ''
+          clang-tidy --warnings-as-errors='*' ../*.c
+          touch $out
+          exit 0
+        '';
+      }
+    );
+  };
+
+  meta = with lib; {
+    description = "XDG Desktop Portal implementation for Spectrum VMs";
+    license = licenses.eupl12;
+    maintainers = with maintainers; [ qyliss ];
+    platforms = platforms.linux;
+  };
+})
+) (_: {})

tools/xdg-desktop-portal-spectrum/meson.build
@@ -1,0 +1,33 @@
+# SPDX-License-Identifier: EUPL-1.2+
+# SPDX-FileCopyrightText: 2024 Alyssa Ross <hi@alyssa.is>
+
+project('xdg-desktop-portal-spectrum', 'c',
+  default_options : {
+    'c_std': 'c2x',
+    'warning_level': '3',
+  })
+
+add_project_arguments('-D_GNU_SOURCE', language : 'c')
+
+dbus = dependency('dbus-1')
+
+install_data('spectrum.portal',
+  install_dir : get_option('datadir') / 'xdg-desktop-portal/portals')
+
+service_conf_data = configuration_data()
+service_conf_data.set('bindir', get_option('prefix') / get_option('bindir'))
+configure_file(
+  input : 'org.freedesktop.impl.portal.desktop.spectrum.service.in',
+  output : 'org.freedesktop.impl.portal.desktop.spectrum.service',
+  configuration : service_conf_data,
+  install_dir : get_option('datadir') / 'dbus-1/services')
+
+exe_conf_data = configuration_data()
+exe_conf_data.set_quoted('HOST_FS_ROOT_DIR', get_option('hostfsrootdir'))
+configure_file(
+  output : 'config.h',
+  configuration : exe_conf_data)
+
+executable('xdg-desktop-portal-spectrum', 'xdg-desktop-portal-spectrum.c',
+  dependencies : dbus,
+  install : true)

tools/xdg-desktop-portal-spectrum/org.freedesktop.impl.portal.desktop.spectrum.service.in
@@ -1,0 +1,6 @@
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: 2024 Alyssa Ross <hi@alyssa.is>
+
+[D-BUS Service]
+Name=org.freedesktop.impl.portal.desktop.spectrum
+Exec=@bindir@/xdg-desktop-portal-spectrum

tools/xdg-desktop-portal-spectrum/shell.nix
@@ -1,0 +1,13 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2024 Alyssa Ross <hi@alyssa.is>
+
+import ../../lib/call-package.nix ({ callSpectrumPackage, clang-tools }:
+
+(callSpectrumPackage ./. {}).overrideAttrs (
+{ hardeningDisable ? [], nativeBuildInputs ? [], ... }:
+
+{
+  hardeningDisable = hardeningDisable ++ [ "fortify" ];
+
+  nativeBuildInputs = nativeBuildInputs ++ [ clang-tools ];
+})) (_: {})

tools/xdg-desktop-portal-spectrum/spectrum.portal
@@ -1,0 +1,6 @@
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: 2024 Alyssa Ross <hi@alyssa.is>
+
+[portal]
+DBusName=org.freedesktop.impl.portal.desktop.spectrum
+Interfaces=org.freedesktop.impl.portal.FileChooser

commit 67607dedfe49839a33c8b1fa2e33d5dfda2a979a

tools/xdg-desktop-portal-spectrum: fix error
Typo ("send" -> "sent"), and tried to print an irrelevant errno value.

Fixes: 03174be ("tools/xdg-desktop-portal-spectrum: init")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

tools/xdg-desktop-portal-spectrum/xdg-desktop-portal-spectrum.c
@@ -109,7 +109,7 @@ static void check_result(int sock)
 	if (read(sock, &r, 1) == -1)
 		err(EXIT_FAILURE, "reading result");
 	if (r)
-		err(EXIT_FAILURE, "host send bad result: %hhd", r);
+		errx(EXIT_FAILURE, "host sent bad result: %hhd", r);
 }
 
 int main(void)