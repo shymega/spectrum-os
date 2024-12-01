shymega/spectrum-os

commit 296ccff852d5de4d6735cd45d038b600df84258d

Build images with __structuredAttrs
This is a requirement to be able to use unsafeDiscardReferences.

Signed-off-by: Alyssa Ross <hi@alyssa.is>

host/initramfs/default.nix
@@ -58,6 +58,7 @@ let
   microcode = if stdenvNoCC.hostPlatform.isx86_64 then
     runCommand "microcode.cpio" {
       nativeBuildInputs = [ cpio ];
+      __structuredAttrs = true;
     } ''
       cpio -id < ${microcodeAmd}/amd-ucode.img
       cpio -id < ${microcodeIntel}/intel-ucode.img
@@ -68,6 +69,7 @@ let
   packagesCpio = runCommand "packages.cpio" {
     nativeBuildInputs = [ cpio ];
     storePaths = writeClosure [ packagesSysroot ];
+    __structuredAttrs = true;
   } ''
     cd ${packagesSysroot}
     (printf "/nix\n/nix/store\n" && find . $(< $storePaths)) |
@@ -87,8 +89,10 @@ stdenvNoCC.mkDerivation {
   };
   sourceRoot = "source/host/initramfs";
 
-  MICROCODE = microcode;
-  PACKAGES_CPIO = packagesCpio;
+  env = {
+    MICROCODE = microcode;
+    PACKAGES_CPIO = packagesCpio;
+  };
 
   nativeBuildInputs = [ cpio lseek ];
 
@@ -97,5 +101,7 @@ stdenvNoCC.mkDerivation {
   dontInstall = true;
 
   enableParallelBuilding = true;
+
+  __structuredAttrs = true;
 }
 ) {}) (_: {})

host/rootfs/default.nix
@@ -199,19 +199,23 @@ stdenvNoCC.mkDerivation {
 
   nativeBuildInputs = [ erofs-utils lseek s6-rc ];
 
-  MODULES_ALIAS = "${kernel}/lib/modules/${kernel.modDirVersion}/modules.alias";
-  MODULES_ORDER = "${kernel}/lib/modules/${kernel.modDirVersion}/modules.order";
+  env = {
+    MODULES_ALIAS = "${kernel}/lib/modules/${kernel.modDirVersion}/modules.alias";
+    MODULES_ORDER = "${kernel}/lib/modules/${kernel.modDirVersion}/modules.order";
 
-  PACKAGES = runCommand "packages" {} ''
-    printf "%s\n/\n" ${packagesSysroot} >$out
-    sed p ${writeClosure [ packagesSysroot] } >>$out
-  '';
+    PACKAGES = runCommand "packages" {} ''
+      printf "%s\n/\n" ${packagesSysroot} >$out
+      sed p ${writeClosure [ packagesSysroot] } >>$out
+    '';
+  };
 
   makeFlags = [ "dest=$(out)" ];
 
   dontInstall = true;
 
   enableParallelBuilding = true;
+
+  __structuredAttrs = true;
 
   passthru = { inherit appvm firmware kernel nixosAllHardware pkgsGui; };
 

img/app/default.nix
@@ -115,11 +115,13 @@ stdenvNoCC.mkDerivation {
 
   nativeBuildInputs = [ erofs-utils jq lseek s6-rc util-linux ];
 
-  KERNEL = "${kernel}/${baseNameOf kernelTarget}";
-  PACKAGES = runCommand "packages" {} ''
-    printf "%s\n/\n" ${packagesSysroot} >$out
-    sed p ${writeClosure [ packagesSysroot] } >>$out
-  '';
+  env = {
+    KERNEL = "${kernel}/${baseNameOf kernelTarget}";
+    PACKAGES = runCommand "packages" {} ''
+      printf "%s\n/\n" ${packagesSysroot} >$out
+      sed p ${writeClosure [ packagesSysroot] } >>$out
+    '';
+  };
 
   makeFlags = [ "prefix=$(out)" ];
 
@@ -128,6 +130,8 @@ stdenvNoCC.mkDerivation {
   enableParallelBuilding = true;
 
   passthru = { inherit appimageFhsenv kernel packagesSysroot; };
+
+  __structuredAttrs = true;
 
   meta = with lib; {
     license = licenses.eupl12;

release/combined/default.nix
@@ -29,6 +29,7 @@ let
 
   rootfs = runCommand "installer.img" {
     nativeBuildInputs = [ squashfs-tools-ng ];
+    __structuredAttrs = true;
   } ''
     sed 's,^${storeDir}/,,' ${writeClosure [ installer.store ]} |
         tar -C ${storeDir} -c --verbatim-files-from -T - \
@@ -65,6 +66,7 @@ let
       "efiemu32.o" "efiemu64.o" "moddep.lst" "command.lst" "fs.lst" "partmap.lst"
       "parttool.lst" "video.lst" "crypto.lst" "terminal.lst" "modinfo.sh"
     ];
+    __structuredAttrs = true;
     passthru = { inherit grubCfg; };
   } ''
     truncate -s 15M $out
@@ -89,6 +91,7 @@ in
 
 runCommand "spectrum-installer" {
   nativeBuildInputs = [ grub jq util-linux systemdMinimal ];
+  __structuredAttrs = true;
   passthru = { inherit eosimages esp installer rootfs; };
 } ''
   blockSize() {

release/live/default.nix
@@ -35,18 +35,22 @@ stdenvNoCC.mkDerivation {
 
   nativeBuildInputs = [ cryptsetup dosfstools jq lseek mtools util-linux ];
 
-  EXT_FS = extfs;
-  INITRAMFS = initramfs;
-  KERNEL = "${rootfs.kernel}/${stdenv.hostPlatform.linux-kernel.target}";
-  ROOT_FS = rootfs;
-  SYSTEMD_BOOT_EFI = "${systemd}/lib/systemd/boot/efi/systemd-boot${efiArch}.efi";
-  EFINAME = "BOOT${toUpper efiArch}.EFI";
+  env = {
+    EXT_FS = extfs;
+    INITRAMFS = initramfs;
+    KERNEL = "${rootfs.kernel}/${stdenv.hostPlatform.linux-kernel.target}";
+    ROOT_FS = rootfs;
+    SYSTEMD_BOOT_EFI = "${systemd}/lib/systemd/boot/efi/systemd-boot${efiArch}.efi";
+    EFINAME = "BOOT${toUpper efiArch}.EFI";
+  };
 
   buildFlags = [ "dest=$(out)" ];
 
   dontInstall = true;
 
   enableParallelBuilding = true;
+
+  __structuredAttrs = true;
 
   passthru = { inherit initramfs rootfs; };
 }

vm/sys/net/default.nix
@@ -101,17 +101,21 @@ stdenvNoCC.mkDerivation {
 
   nativeBuildInputs = [ erofs-utils jq lseek s6-rc util-linux ];
 
-  KERNEL = "${kernel}/${baseNameOf kernelTarget}";
-  PACKAGES = runCommand "packages" {} ''
-    printf "%s\n/\n" ${packagesSysroot} >$out
-    sed p ${writeClosure [ packagesSysroot] } >>$out
-  '';
+  env = {
+    KERNEL = "${kernel}/${baseNameOf kernelTarget}";
+    PACKAGES = runCommand "packages" {} ''
+      printf "%s\n/\n" ${packagesSysroot} >$out
+      sed p ${writeClosure [ packagesSysroot] } >>$out
+    '';
+  };
 
   makeFlags = [ "prefix=$(out)" ];
 
   dontInstall = true;
 
   enableParallelBuilding = true;
+
+  __structuredAttrs = true;
 
   passthru = { inherit kernel; };
 

host/initramfs/extfs.nix
@@ -15,6 +15,7 @@ in
 
 runCommand "ext.ext4" {
   nativeBuildInputs = [ e2fsprogs ];
+  __structuredAttrs = true;
 } ''
   mkdir -p root/svc/data/appvm-{firefox,foot,gnome-text-editor}
   cd root

release/combined/eosimages.nix
@@ -8,6 +8,7 @@ runCommand "eosimages.img" {
   nativeBuildInputs = [ e2fsprogs tar2ext4 ];
   imageName = "Spectrum-0.0-x86_64-generic.0.Live.img";
   image = callSpectrumPackage ../live {};
+  __structuredAttrs = true;
 } ''
   mkdir dir
   cd dir

commit b0b128c12eaafdf3b8e6b1a6f11502c9c17032a3

host/initramfs: fix non-x86_64 env error
With __structuredAttrs, it's no longer valid for MICROCODE to be set
to null — we need to avoid setting it if there's no value to go in it.

Fixes: 296ccff ("Build images with __structuredAttrs")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

host/initramfs/default.nix
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2021-2023 Alyssa Ross <hi@alyssa.is>
+# SPDX-FileCopyrightText: 2021-2024 Alyssa Ross <hi@alyssa.is>
 # SPDX-License-Identifier: MIT
 
 import ../../lib/call-package.nix (
@@ -55,17 +55,15 @@ let
     cp ${util-linuxMinimal}/bin/{findfs,lsblk} $out/bin
   '';
 
-  microcode = if stdenvNoCC.hostPlatform.isx86_64 then
-    runCommand "microcode.cpio" {
-      nativeBuildInputs = [ cpio ];
-      __structuredAttrs = true;
-      unsafeDiscardReferences = { out = true; };
-    } ''
-      cpio -id < ${microcodeAmd}/amd-ucode.img
-      cpio -id < ${microcodeIntel}/intel-ucode.img
-      find kernel | cpio -oH newc -R +0:+0 --reproducible > $out
-    ''
-  else null;
+  microcode = runCommand "microcode.cpio" {
+    nativeBuildInputs = [ cpio ];
+    __structuredAttrs = true;
+    unsafeDiscardReferences = { out = true; };
+  } ''
+    cpio -id < ${microcodeAmd}/amd-ucode.img
+    cpio -id < ${microcodeIntel}/intel-ucode.img
+    find kernel | cpio -oH newc -R +0:+0 --reproducible > $out
+  '';
 
   packagesCpio = runCommand "packages.cpio" {
     nativeBuildInputs = [ cpio ];
@@ -92,8 +90,9 @@ stdenvNoCC.mkDerivation {
   sourceRoot = "source/host/initramfs";
 
   env = {
-    MICROCODE = microcode;
     PACKAGES_CPIO = packagesCpio;
+  } // lib.optionalAttrs stdenvNoCC.hostPlatform.isx86_64 {
+    MICROCODE = microcode;
   };
 
   nativeBuildInputs = [ cpio lseek ];