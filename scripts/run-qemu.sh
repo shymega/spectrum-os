shymega/spectrum-os

commit 6c073176f5d9a803fb94d628b55d6dbaf673bc32

vm: support "make run" on aarch64
Signed-off-by: Alyssa Ross <hi@alyssa.is>

scripts/run-qemu.sh
@@ -1,0 +1,36 @@
+#!/bin/sh -ue
+# SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
+# SPDX-License-Identifier: EUPL-1.2+
+
+# This script wraps around QEMU to paper over platform differences,
+# which can't be handled portably in Make language.
+
+machine=virt
+
+if [ "${ARCH:="$(uname -m)"}" = x86_64 ]; then
+	append="console=ttyS0${append:+ $append}"
+	machine=q35
+fi
+
+i=0
+while [ $i -lt $# ]; do
+	arg="$1"
+	shift
+
+	if [ "$arg" = -append ]; then
+		append="${append:+$append }$1"
+		shift
+		continue
+	fi
+
+	set -- "$@" "$arg"
+
+	i=$((i + 1))
+done
+
+set -x
+exec ${QEMU_SYSTEM:-qemu-system-$ARCH} \
+	-accel kvm \
+	-machine "$machine" \
+	${append:+-append "$append"} \
+	"$@"

img/app/Makefile
@@ -87,10 +87,10 @@ start-virtiofsd:
 .PHONY: start-virtiofsd
 
 run-qemu: $(imgdir)/appvm/blk/root.img start-virtiofsd
-	$(QEMU_KVM) -m 128 -cpu host -machine q35,kernel=$(KERNEL) -vga none \
+	@../../scripts/run-qemu.sh -m 128 -cpu host -kernel $(KERNEL) -vga none \
 	    -drive file=$(imgdir)/appvm/blk/root.img,if=virtio,format=raw,readonly=on \
 	    -drive file=$(RUN_IMG),if=virtio,format=raw,readonly=on \
-	    -append "console=ttyS0 root=PARTLABEL=root" \
+	    -append "root=PARTLABEL=root" \
 	    -netdev user,id=net0 \
 	    -device virtio-net,netdev=net0,mac=0A:B3:EC:00:00:00 \
 	    -chardev socket,id=virtiofsd,path=build/virtiofsd.sock \
@@ -104,14 +104,14 @@ run-qemu: $(imgdir)/appvm/blk/root.img start-virtiofsd
 
 run-cloud-hypervisor: $(imgdir)/appvm/blk/root.img start-virtiofsd
 	rm -f build/vmm.sock
-	$(CLOUD_HYPERVISOR) \
+	@../../scripts/run-cloud-hypervisor.sh \
 	    --api-socket path=build/vmm.sock \
 	    --memory size=128M,shared=on \
 	    --disk path=$(imgdir)/appvm/blk/root.img,readonly=on \
 	    --disk path=$(RUN_IMG),readonly=on \
 	    --fs tag=virtiofs0,socket=build/virtiofsd.sock \
 	    --kernel $(KERNEL) \
-	    --cmdline "console=ttyS0 root=PARTLABEL=root" \
+	    --cmdline "root=PARTLABEL=root" \
 	    --console tty \
 	    --serial pty
 .PHONY: run-cloud-hypervisor

lib/common.mk
@@ -9,8 +9,6 @@
 QEMU_KVM = qemu-kvm
 
 BACKGROUND = background
-CLOUD_HYPERVISOR = cloud-hypervisor
-CLOUD_HYPERVISOR = cloud-hypervisor
 CPIO = cpio
 CPIOFLAGS = --reproducible -R +0:+0 -H newc
 MCOPY = mcopy

scripts/run-cloud-hypervisor.sh
@@ -1,0 +1,29 @@
+#!/bin/sh -ue
+# SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
+# SPDX-License-Identifier: EUPL-1.2+
+
+# This script wraps around cloud-hypervisor to paper over platform
+# differences, which can't be handled portably in Make language.
+
+if [ "${ARCH:="$(uname -m)"}" = x86_64 ]; then
+	cmdline="console=ttyS0${cmdline:+ $cmdline}"
+fi
+
+i=0
+while [ $i -lt $# ]; do
+	arg="$1"
+	shift
+
+	if [ "$arg" = --cmdline ]; then
+		cmdline="${cmdline:+$cmdline }$1"
+		shift
+		continue
+	fi
+
+	set -- "$@" "$arg"
+
+	i=$((i + 1))
+done
+
+set -x
+exec ${CLOUD_HYPERVISOR:-cloud-hypervisor} ${cmdline:+--cmdline "$cmdline"} "$@"

vm/sys/net/Makefile
@@ -95,9 +95,9 @@ build/etc/s6-rc: $(VM_S6_RC_FILES)
 	    exit=$$?; rm -r $$dir; exit $$exit
 
 run-qemu: build/host/data/netvm/blk/root.img
-	$(QEMU_KVM) -m 128 -cpu host -machine q35,kernel=$(KERNEL) -vga none \
+	@../../../scripts/run-qemu.sh -m 128 -cpu host -kernel $(KERNEL) -vga none \
 	    -drive file=build/host/data/netvm/blk/root.img,if=virtio,format=raw,readonly=on \
-	    -append "console=ttyS0 root=PARTLABEL=root" \
+	    -append "root=PARTLABEL=root" \
 	    -netdev user,id=net0 \
 	    -device e1000e,netdev=net0 \
 	    -netdev user,id=net1 \
@@ -109,14 +109,14 @@ run-qemu: build/host/data/netvm/blk/root.img
 
 run-cloud-hypervisor: build/host/data/netvm/blk/root.img
 	rm -f build/vmm.sock
-	$(CLOUD_HYPERVISOR) \
+	@../../../scripts/run-cloud-hypervisor.sh \
 	    --api-socket path=build/vmm.sock \
 	    --memory size=128M \
 	    --disk path=build/host/data/netvm/blk/root.img,readonly=on \
 	    --net tap=tap0 \
 	    --net tap=tap1,mac=0A:B3:EC:80:00:00 \
 	    --kernel $(KERNEL) \
-	    --cmdline "console=ttyS0 root=PARTLABEL=root" \
+	    --cmdline "root=PARTLABEL=root" \
 	    --console tty \
 	    --serial pty
 .PHONY: run-cloud-hypervisor

commit 621aecb01fd18acdd03e8afee881a61fc8392602

scripts/run-qemu.sh: mess with -append less
Now, the modified -append will stay in its original place on the
command line, and if -append is supplied multiple times, they'll each
be modified and passed to QEMU, rather than being merged together.

Fixes: 6c07317 ("vm: support "make run" on aarch64")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

scripts/run-qemu.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ue
-# SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
+# SPDX-FileCopyrightText: 2023-2024 Alyssa Ross <hi@alyssa.is>
 # SPDX-License-Identifier: EUPL-1.2+
 
 # This script wraps around QEMU to paper over platform differences,
@@ -18,7 +18,8 @@ while [ $i -lt $# ]; do
 	shift
 
 	if [ "$arg" = -append ]; then
-		append="${append:+$append }$1"
+		set -- "$@" -append "${append:+$append }$1"
+		i=$((i + 2))
 		shift
 		continue
 	fi
@@ -26,6 +27,12 @@ while [ $i -lt $# ]; do
 	set -- "$@" "$arg"
 
 	i=$((i + 1))
+done
+
+for arg; do
+	if [ "$arg" = -append ]; then
+		append=
+	fi
 done
 
 set -x