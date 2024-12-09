shymega/spectrum-os

commit 6b34ec8a8ba7c48837edcec06be808d773ce793a

release/checks/try.nix: init
This is a regression test for c7f87f3 ("host/rootfs: allow growing ext
partition to fail").  It's the first time we're actually doing
automated tests of a Spectrum boot.  For now, I'm using the NixOS test
framework, but because we're not using NixOS and not setting any NixOS
options, it feels to me like it doesn't actually buy us very much, so
if it doesn't start adding more value as we add more (or more complex)
tests, it might be simpler to just use a shell/execline script for
tests.

Signed-off-by: Alyssa Ross <hi@alyssa.is>
Message-Id: <20230526210757.397735-1-hi@alyssa.is>
Reviewed-by: Ryan Lahfa <ryan@lahfa.xyz>

release/checks/try.nix
@@ -1,0 +1,51 @@
+# SPDX-License-Identifier: MIT
+# SPDX-FileCopyrightText: 2023 Alyssa Ross <hi@alyssa.is>
+
+import ../../lib/eval-config.nix ({ config, ... }:
+
+let
+  live = import ../live { inherit config; };
+in
+
+config.pkgs.nixosTest ({ stdenv, mtools, ... }: {
+  name = "try-spectrum-test";
+  nodes = {};
+
+  testScript = ''
+    import shlex
+    import subprocess
+
+    conf = subprocess.run([
+      "${mtools}/bin/mcopy",
+      "-i",
+      "${live}@@1M",
+      "::loader/entries/spectrum.conf",
+      "-",
+    ], stdout=subprocess.PIPE)
+    conf.check_returncode()
+
+    cmdline = None
+    for line in conf.stdout.decode('utf-8').splitlines():
+      key, value = line.split(' ', 1)
+      if key == 'options':
+        cmdline = value
+        break
+
+    flags = " ".join(map(shlex.quote, [
+      "qemu-kvm",
+      "-m", "512",
+      "-kernel", "${live.rootfs.kernel}/${stdenv.hostPlatform.linux-kernel.target}",
+      "-initrd", "${live.initramfs}",
+      "-device", "qemu-xhci",
+      "-device", "usb-storage,drive=drive1,removable=true",
+      "-drive", "file=${live},id=drive1,format=raw,if=none,readonly=on",
+      "-append", f"panic=-1 {cmdline}",
+    ]))
+
+    machine = create_machine({"startCommand": flags})
+
+    machine.start()
+    machine.wait_for_console_text("EXT4-fs \(sda4\): mounted filesystem")
+    machine.crash()
+  '';
+}))

release/checks/default.nix
@@ -15,4 +15,6 @@ import ../../lib/eval-config.nix ({ ... } @ args:
   rustfmt = import ./rustfmt.nix args;
 
   shellcheck = import ./shellcheck.nix args;
+
+  try = import ./try.nix args;
 })

release/live/default.nix
@@ -48,6 +48,6 @@ stdenvNoCC.mkDerivation {
 
   enableParallelBuilding = true;
 
-  passthru = { inherit rootfs; };
+  passthru = { inherit initramfs rootfs; };
 }
 ) {})

commit 1c26006f409210131bbcce5cedb0c018421e5958

release/checks/try.nix: fix SyntaxWarning
<string>:34: SyntaxWarning: invalid escape sequence '\('

Fixes: 6b34ec8 ("release/checks/try.nix: init")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

release/checks/try.nix
@@ -45,7 +45,7 @@ nixosTest ({ stdenv, mtools, ... }: {
     machine = create_machine(flags)
 
     machine.start()
-    machine.wait_for_console_text("EXT4-fs \(sda4\): mounted filesystem")
+    machine.wait_for_console_text("EXT4-fs \\(sda4\\): mounted filesystem")
     machine.crash()
   '';
 })) (_: {})