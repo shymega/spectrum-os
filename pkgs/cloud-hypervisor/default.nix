62a8b0c79a31cad13b0bea10fc8abf30bd6684a2
Alyssa Ross <hi@alyssa.is>
2024-12-20 21:33:12 +0100

    lib/nixpkgs.default.nix: update to master
    Updating to master to get the updated Cloud Hypervisor patches out.
    
    Signed-off-by: Alyssa Ross <hi@alyssa.is>

--- pkgs/cloud-hypervisor/default.nix
+++ pkgs/cloud-hypervisor/default.nix
@@ -12,9 +12,8 @@ super.cloud-hypervisor.overrideAttrs (
       outputHashes = {
         "acpi_tables-0.1.0" = "sha256-ReIibUCFiLVq6AFqFupue/3BEQUJoImCLKaUBSVpdl4=";
         "micro_http-0.1.0" = "sha256-yIgcoEfc7eeS1+bijzkifaBxVNHa71Y+Vn79owMaKvM=";
-        "mshv-bindings-0.3.0" = "sha256-IqmFB4nyENsfEPqiSYv52sL4LDiv+rCabTiIxE1MWZ0=";
-        "vfio-bindings-0.4.0" = "sha256-ie/RcYbojLCGJkc6Yl97iUhOxnYk8/DO7JKlhMtT/6w=";
-        "vfio_user-0.1.0" = "sha256-jScCwZEqoWYGBBKjoxB6xXOltX1/5h4Jgpcy5RzzTtg=";
+        "vfio-bindings-0.4.0" = "sha256-hC5BlvXYiQJg3wnq7awIDGYpLK8ED2A7L8GgpvQKXqw";
+        "vfio_user-0.1.0" = "sha256-hlK3LO/WBvNP7CqxJSV+aQO1rrtwNfmUz9VMWTk3TCc=";
         "vm-fdt-0.3.0" = "sha256-9PywgSnSL+8gT6lcl9t6w7X4fEINa+db+H1vWS+gDOI=";
       };
     };
@@ -23,8 +22,8 @@ super.cloud-hypervisor.overrideAttrs (
       name = "vhost";
       owner = "rust-vmm";
       repo = "vhost";
-      rev = "vhost-user-backend-v0.16.0";
-      hash = "sha256-ol1QacoMD8E85uaNrCr7Vy92XjZfQCB9FbZsAEvq2fM=";
+      rev = "d983ae07f78663b7d24059667376992460b571a2";
+      hash = "sha256-tSP8Ent7URu/6ehOOMP29ryLfV465ip2xrXkKu2nLYI=";
     };
 
     patches = patches ++ [
