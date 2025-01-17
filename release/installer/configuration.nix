shymega/spectrum-os

commit 434ce2ad838f961dc1e5643a73aec75026ddbdb5

release/installer: delay cage until card0
Otherwise, cage might be started too early, and we'll get errors like
this:

	00:00:10.413 [ERROR] [backend/backend.c:253] Found 0 GPUs, cannot create backend
	00:00:10.414 [ERROR] [backend/backend.c:428] Failed to open any DRM device
	00:00:10.490 [ERROR] [../cage.c:318] Unable to create the wlroots backend

Fixes: c266b12 ("configuration.nix: remove GNOME+installer stuff")
Signed-off-by: Alyssa Ross <hi@alyssa.is>

release/installer/configuration.nix
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: MIT
-# SPDX-FileCopyrightText: 2021-2023 Alyssa Ross <hi@alyssa.is>
+# SPDX-FileCopyrightText: 2021-2024 Alyssa Ross <hi@alyssa.is>
 
 { lib, modulesPath, pkgs, ... }:
 
@@ -34,6 +34,13 @@ in
 
   services.cage.enable = true;
   services.cage.program = lib.getExe (pkgs.callPackage ./app {});
+
+  services.udev.extraRules = ''
+    KERNEL=="card0", TAG+="systemd"
+  '';
+
+  systemd.services.cage-tty1.after = [ "dev-dri-card0.device" ];
+  systemd.services.cage-tty1.wants = [ "dev-dri-card0.device" ];
 
   # Force eos-installer to stop artificially constraining its size.
   systemd.services.cage-tty1.environment.GIS_SMALL_SCREEN = "1";

commit 3f83b083cbc7676d850d63d65a21cf2e97927fe9

release/installer: fix copyright year
I forgot that the year had changed…

Fixes: 434ce2a ("release/installer: delay cage until card0")
Fixes: 50c8db4 ("release/installer/run-vm.nix: log to console")

release/installer/configuration.nix
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: MIT
-# SPDX-FileCopyrightText: 2021-2024 Alyssa Ross <hi@alyssa.is>
+# SPDX-FileCopyrightText: 2021-2023, 2025 Alyssa Ross <hi@alyssa.is>
 
 { lib, modulesPath, pkgs, ... }:
 

release/installer/run-vm.nix
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: MIT
-# SPDX-FileCopyrightText: 2021-2024 Alyssa Ross <hi@alyssa.is>
+# SPDX-FileCopyrightText: 2021-2025 Alyssa Ross <hi@alyssa.is>
 
 import ../../lib/call-package.nix (
 { callSpectrumPackage, lib, coreutils, qemu_kvm, stdenv, writeShellScript }: